3c8a7ef31f93661106c561642d6aa87f44e72b49
Dave Moten <davidmoten@gmail.com>
2018-09-06 17:11:03 +1000

    add ascii chec

--- odata-client-core/src/main/java/com/github/davidmoten/odata/client/EntityPreconditions.java
+++ odata-client-core/src/main/java/com/github/davidmoten/odata/client/EntityPreconditions.java
@@ -4,6 +4,7 @@ import java.nio.charset.StandardCharsets;
 
 public class EntityPreconditions {
 
+    //TODO why not used in generated classes? 
     public static void checkIsAscii(String v) {
         if (!StandardCharsets.US_ASCII.newEncoder().canEncode(v)) {
             throw new RuntimeException("illegal encoding, must be ascii");
