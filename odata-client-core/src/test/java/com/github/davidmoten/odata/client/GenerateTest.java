e69d8e6f58e8c0d8364ba6d996d05531b3f42a4d
Dave Moten <davidmoten@gmail.com>
2018-09-06 16:39:29 +1000

    test can method chain with generated object

--- odata-client-core/src/test/java/com/github/davidmoten/odata/client/GenerateTest.java
+++ odata-client-core/src/test/java/com/github/davidmoten/odata/client/GenerateTest.java
@@ -1,5 +1,7 @@
 package com.github.davidmoten.odata.client;
 
+import java.util.Optional;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
@@ -8,6 +10,8 @@ import javax.xml.transform.stream.StreamSource;
 import org.junit.Test;
 import org.oasisopen.odata.csdl.v4.TDataServices;
 import org.oasisopen.odata.csdl.v4.TEdmx;
+
+import microsoft.graph.generated.entity.ActivityHistoryItem;
 
 public class GenerateTest {
 
@@ -21,5 +25,10 @@ public class GenerateTest {
                 .getValue();
         Generator g = new Generator(new Options(), t.getDataServices().getSchema().get(0));
         g.generate();
+    }
+    
+    @Test
+    public void generatedObjectCanMethodChain() {
+        new ActivityHistoryItem().setActiveDurationSeconds(Optional.empty()).setCreatedDateTime(Optional.empty());
     }
 }
