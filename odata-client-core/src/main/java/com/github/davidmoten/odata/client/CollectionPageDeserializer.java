1f44d4e16fffc73d9fc866825c582f8fb7326ff5
Dave Moten <davidmoten@gmail.com>
2018-09-07 14:49:50 +1000

    collection request dev

--- odata-client-core/src/main/java/com/github/davidmoten/odata/client/CollectionPageDeserializer.java
+++
@@ -1,5 +0,0 @@
-package com.github.davidmoten.odata.client;
-
-public class CollectionPageDeserializer {
-
-}
---
+++ odata-client-core/src/main/java/com/github/davidmoten/odata/client/QueryOption.java
@@ -0,0 +1,21 @@
+package com.github.davidmoten.odata.client;
+
+public final class QueryOption {
+    
+    private final String name;
+    private final String value;
+
+    public QueryOption(String name, String value) {
+        this.name = name;
+        this.value = value;
+    }
+
+    public String name() {
+        return name;
+    }
+
+    public String value() {
+        return value;
+    }
+    
+}
