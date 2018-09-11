51b73abadf6c41f15212cbdfda71f7af24d1ffce
Dave Moten <davidmoten@gmail.com>
2018-09-11 20:00:57 +1000

    add TODO

--- odata-client-core/src/main/java/com/github/davidmoten/odata/client/EntityRequest.java
+++ odata-client-core/src/main/java/com/github/davidmoten/odata/client/EntityRequest.java
@@ -2,6 +2,7 @@ package com.github.davidmoten.odata.client;
 
 public interface EntityRequest<T extends ODataEntity> {
 
+    // TODO customize HTTP headers, add delete, update, patch, select, expand
     T get(QueryOption... options);
-    
+
 }
