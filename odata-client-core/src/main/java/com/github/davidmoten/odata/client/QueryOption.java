4698a5f8b544ec3b3e4de3170a81c1f5d1e98046
Dave Moten <davidmoten@gmail.com>
2018-09-12 10:26:26 +1000

    collection request options dev

--- odata-client-core/src/main/java/com/github/davidmoten/odata/client/QueryOption.java
+++ odata-client-core/src/main/java/com/github/davidmoten/odata/client/QueryOption.java
@@ -2,8 +2,5 @@ package com.github.davidmoten.odata.client;
 
 public interface QueryOption {
 
-    public static CollectionRequestOptionsBuilder requestHeader(String key, String value) {
-        return new CollectionRequestOptionsBuilder().requestHeader(key, value);
-    }
 
 }
