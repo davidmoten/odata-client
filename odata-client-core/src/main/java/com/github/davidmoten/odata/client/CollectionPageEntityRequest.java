8c3fae869d791c396354646f8c8b311c3d79a121
Dave Moten <davidmoten@gmail.com>
2018-09-12 10:11:29 +1000

    collection request options dev

--- odata-client-core/src/main/java/com/github/davidmoten/odata/client/CollectionPageEntityRequest.java
+++ odata-client-core/src/main/java/com/github/davidmoten/odata/client/CollectionPageEntityRequest.java
@@ -2,8 +2,32 @@ package com.github.davidmoten.odata.client;
 
 public interface CollectionPageEntityRequest<T extends ODataEntity, R extends EntityRequest<T>> {
 
-    CollectionPage<T> get(QueryOption... options);
-    
+    CollectionPage<T> get(CollectionRequestOptions options);
+
     R id(String id);
-    
+
+    default CollectionRequestOptionsBuilder<T, R> requestHeader(String key, String value) {
+        return new CollectionRequestOptionsBuilder<T, R>(this).requestHeader(key, value);
+    }
+
+    default CollectionRequestOptionsBuilder<T, R> search(String clause) {
+        return new CollectionRequestOptionsBuilder<T, R>(this).search(clause);
+    }
+
+    default CollectionRequestOptionsBuilder<T, R> filter(String clause) {
+        return new CollectionRequestOptionsBuilder<T, R>(this).filter(clause);
+    }
+
+    default CollectionRequestOptionsBuilder<T, R> orderBy(String clause) {
+        return new CollectionRequestOptionsBuilder<T, R>(this).orderBy(clause);
+    }
+
+    default CollectionRequestOptionsBuilder<T, R> skip(long n) {
+        return new CollectionRequestOptionsBuilder<T, R>(this).skip(n);
+    }
+
+    default CollectionRequestOptionsBuilder<T, R> top(long n) {
+        return new CollectionRequestOptionsBuilder<T, R>(this).top(n);
+    }
+
 }
