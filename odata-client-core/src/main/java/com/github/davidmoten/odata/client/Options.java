0f3f445fdc5c94fecb470cfd782bc32e9e1fa607
Dave Moten <davidmoten@gmail.com>
2018-09-12 16:15:35 +1000

    options builder

--- odata-client-core/src/main/java/com/github/davidmoten/odata/client/Options.java
+++ odata-client-core/src/main/java/com/github/davidmoten/odata/client/Options.java
@@ -1,45 +1,182 @@
 package com.github.davidmoten.odata.client;
 
+import java.util.Optional;
+
+
 public final class Options {
 
+    private final String outputDirectory;
+    private final String pkg;
+    private final String packageSuffixEnum;
+    private final String packageSuffixEntity;
+    private final String packageSuffixComplexType;
+    private final String packageSuffixEntityRequest;
+    private final String packageSuffixCollectionRequest;
+    private final String packageSuffixContainer;
+    private final String collectionRequestClassSuffix;
+    private final String entityRequestClassSuffix;
+
+    private Options(String outputDirectory, String pkg, String packageSuffixEnum, String packageSuffixEntity, String packageSuffixComplexType, String packageSuffixEntityRequest, String packageSuffixCollectionRequest, String packageSuffixContainer, String collectionRequestClassSuffix, String entityRequestClassSuffix) {
+        notNull(outputDirectory, "outputDirectory");
+        notNull(pkg, "pkg");
+        notNull(packageSuffixEnum, "packageSuffixEnum");
+        notNull(packageSuffixEntity, "packageSuffixEntity");
+        notNull(packageSuffixComplexType, "packageSuffixComplexType");
+        notNull(packageSuffixEntityRequest, "packageSuffixEntityRequest");
+        notNull(packageSuffixCollectionRequest, "packageSuffixCollectionRequest");
+        notNull(packageSuffixContainer, "packageSuffixContainer");
+        notNull(collectionRequestClassSuffix, "collectionRequestClassSuffix");
+        notNull(entityRequestClassSuffix, "entityRequestClassSuffix");
+        this.outputDirectory = outputDirectory;
+        this.pkg = pkg;
+        this.packageSuffixEnum = packageSuffixEnum;
+        this.packageSuffixEntity = packageSuffixEntity;
+        this.packageSuffixComplexType = packageSuffixComplexType;
+        this.packageSuffixEntityRequest = packageSuffixEntityRequest;
+        this.packageSuffixCollectionRequest = packageSuffixCollectionRequest;
+        this.packageSuffixContainer = packageSuffixContainer;
+        this.collectionRequestClassSuffix = collectionRequestClassSuffix;
+        this.entityRequestClassSuffix = entityRequestClassSuffix;
+    }
+
+    public static Builder1 builder() {
+        return new Builder1();
+    }
+
     public String outputDirectory() {
-        return "target/generated-sources/odata";
+        return outputDirectory;
     }
 
     public String pkg() {
-        return "microsoft.graph.generated";
+        return pkg;
     }
 
     public String packageSuffixEnum() {
-        return ".enums";
+        return packageSuffixEnum;
     }
 
     public String packageSuffixEntity() {
-        return ".entity";
+        return packageSuffixEntity;
     }
 
     public String packageSuffixComplexType() {
-        return ".complex";
-    }
-
-    public String packageSuffixCollectionRequest() {
-        return ".collection.request";
+        return packageSuffixComplexType;
     }
 
     public String packageSuffixEntityRequest() {
-        return ".entity.request";
+        return packageSuffixEntityRequest;
     }
 
-    public String collectionRequestClassSuffix() {
-        return "CollectionRequest";
+    public String packageSuffixCollectionRequest() {
+        return packageSuffixCollectionRequest;
     }
 
     public String packageSuffixContainer() {
-        return ".container";
+        return packageSuffixContainer;
+    }
+
+    public String collectionRequestClassSuffix() {
+        return collectionRequestClassSuffix;
     }
 
     public String entityRequestClassSuffix() {
-        return "Request";
+        return entityRequestClassSuffix;
     }
 
+    public static final class Builder1 {
+
+        private String outputDirectory = "target/generated-sources/odata";
+        private String pkg;
+        private String packageSuffixEnum = ".enums";
+        private String packageSuffixEntity = ".entity";
+        private String packageSuffixComplexType = ".complex";
+        private String packageSuffixEntityRequest = ".entity.request";
+        private String packageSuffixCollectionRequest = ".collection.request";
+        private String packageSuffixContainer = ".container";
+        private String collectionRequestClassSuffix = "CollectionRequest";
+        private String entityRequestClassSuffix = "Request";
+
+        Builder1(){
+        }
+
+        public Builder2 pkg(String pkg) {
+            notNull(pkg, "pkg");
+            this.pkg = pkg;
+            return new Builder2(this);
+        }
+
+    }
+
+    public static final class Builder2 {
+
+        private final Builder1 b;
+
+        Builder2(Builder1 b) {
+             this.b = b;
+        }
+
+        public Builder2 outputDirectory(String outputDirectory) {
+            notNull(outputDirectory, "outputDirectory");
+            b.outputDirectory = outputDirectory;
+            return this;
+        }
+
+        public Builder2 packageSuffixEnum(String packageSuffixEnum) {
+            notNull(packageSuffixEnum, "packageSuffixEnum");
+            b.packageSuffixEnum = packageSuffixEnum;
+            return this;
+        }
+
+        public Builder2 packageSuffixEntity(String packageSuffixEntity) {
+            notNull(packageSuffixEntity, "packageSuffixEntity");
+            b.packageSuffixEntity = packageSuffixEntity;
+            return this;
+        }
+
+        public Builder2 packageSuffixComplexType(String packageSuffixComplexType) {
+            notNull(packageSuffixComplexType, "packageSuffixComplexType");
+            b.packageSuffixComplexType = packageSuffixComplexType;
+            return this;
+        }
+
+        public Builder2 packageSuffixEntityRequest(String packageSuffixEntityRequest) {
+            notNull(packageSuffixEntityRequest, "packageSuffixEntityRequest");
+            b.packageSuffixEntityRequest = packageSuffixEntityRequest;
+            return this;
+        }
+
+        public Builder2 packageSuffixCollectionRequest(String packageSuffixCollectionRequest) {
+            notNull(packageSuffixCollectionRequest, "packageSuffixCollectionRequest");
+            b.packageSuffixCollectionRequest = packageSuffixCollectionRequest;
+            return this;
+        }
+
+        public Builder2 packageSuffixContainer(String packageSuffixContainer) {
+            notNull(packageSuffixContainer, "packageSuffixContainer");
+            b.packageSuffixContainer = packageSuffixContainer;
+            return this;
+        }
+
+        public Builder2 collectionRequestClassSuffix(String collectionRequestClassSuffix) {
+            notNull(collectionRequestClassSuffix, "collectionRequestClassSuffix");
+            b.collectionRequestClassSuffix = collectionRequestClassSuffix;
+            return this;
+        }
+
+        public Builder2 entityRequestClassSuffix(String entityRequestClassSuffix) {
+            notNull(entityRequestClassSuffix, "entityRequestClassSuffix");
+            b.entityRequestClassSuffix = entityRequestClassSuffix;
+            return this;
+        }
+
+        public Options build() {
+            return new Options(b.outputDirectory, b.pkg, b.packageSuffixEnum, b.packageSuffixEntity, b.packageSuffixComplexType, b.packageSuffixEntityRequest, b.packageSuffixCollectionRequest, b.packageSuffixContainer, b.collectionRequestClassSuffix, b.entityRequestClassSuffix);
+        }
+    }
+
+    private static void notNull(Object o, String name) {
+        if (o == null) {
+            throw new NullPointerException(name + " cannot be null");
+        }
+    }
 }
---
+++ odata-client-maven-plugin/src/main/java/org/davidmoten/odata/client/maven/GeneratorMojo.java
@@ -0,0 +1,85 @@
+package org.davidmoten.odata.client.maven;
+
+import java.awt.Color;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
+
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+
+import com.github.davidmoten.fsm.graph.NodeOptions;
+import com.github.davidmoten.fsm.model.StateMachineDefinition;
+
+@Mojo(name = "generate", defaultPhase = LifecyclePhase.GENERATE_SOURCES)
+public class GeneratorMojo extends AbstractMojo {
+
+    @Parameter(name = "supplierClass", required = true)
+    String supplierClass;
+
+    @Parameter(name = "packageName", required = true)
+    String packageName;
+
+    @Parameter(name = "outputDirectory", defaultValue = "${project.build.directory}/generated-sources/java")
+    File outputDirectory;
+
+    @Parameter(name = "diagramsDirectory", defaultValue = "${project.build.directory}/state-machine-docs")
+    File diagramsDirectory;
+
+    @Parameter(name = "htmlDirectory", defaultValue = "${project.build.directory}/state-machine-docs")
+    File htmlDirectory;
+
+    @Parameter
+    Map<String, String> stateMachines;
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public void execute() throws MojoExecutionException {
+        Options options = new Options() {};
+        Generator g = new Generator(options);
+    }
+
+    private void generateHtml(StateMachineDefinition<?> machine) {
+        htmlDirectory.mkdirs();
+        File gml = new File(htmlDirectory,
+                machine.cls().getCanonicalName().replace("$", ".") + ".html");
+        try (PrintWriter out = new PrintWriter(gml)) {
+            out.println(machine.documentationHtml());
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        getLog().info("generated graphml file for import to yed (for instance): " + gml);
+
+    }
+
+    private void generateGraphml(StateMachineDefinition<?> machine, boolean includeDocumentation) {
+        diagramsDirectory.mkdirs();
+        File gml = new File(diagramsDirectory, machine.cls().getCanonicalName().replace("$", ".")
+                + (includeDocumentation ? "-with-docs" : "") + ".graphml");
+        try (PrintWriter out = new PrintWriter(gml)) {
+            int nodeWidth = Integer.parseInt(value(machine, "nodeWidth", "280"));
+            int nodeHeight = Integer.parseInt(value(machine, "nodeHeight", "150"));
+            String nodeBackgroundColor = value(machine, "nodeBackgroundColor", "#F3F2C0");
+            out.println(machine.graphml((node -> new NodeOptions(nodeWidth, nodeHeight,
+                    Color.decode(nodeBackgroundColor))), includeDocumentation));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        getLog().info("generated graphml file for import to yed (for instance): " + gml);
+    }
+
+    private String value(StateMachineDefinition<?> machine, String key, String defaultValue) {
+        String value = stateMachines.get(machine.cls().getSimpleName() + "." + key);
+        if (value == null) {
+            return defaultValue;
+        } else {
+            return value;
+        }
+    }
+}
