8a80d58e2d5f95bf0cb4cf5665c93658606b4e68
Dave Moten <davidmoten@gmail.com>
2018-09-15 09:47:26 +1000

    add maven plugin and test project

--- odata-client-core/src/main/other/MsGraphTest.java
+++
@@ -1,9 +0,0 @@
-import microsoft.graph.generated.container.GraphService;
-
-public class MsGraphTest {
-    
-    public void test() {
-        new GraphService(null).users("1").contacts("1").get();
-    }
-
-}
