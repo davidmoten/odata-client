21fbbd4fa4e5e37de731b7601c3f4057dff3fe1b
Dave Moten <davidmoten@gmail.com>
2018-09-16 21:30:18 +1000

    add Indent tests and dev on collections of properties

---
+++ odata-client-core/src/test/java/com/github/davidmoten/odata/client/generator/IndentTest.java
@@ -0,0 +1,22 @@
+package com.github.davidmoten.odata.client.generator;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class IndentTest {
+
+    
+    @Test(expected=RuntimeException.class)
+    public void testMoreLeftThanRight() {
+        Indent indent = new Indent();
+        indent.left();
+    }
+    
+    @Test
+    public void testRightThenLeft() {
+        Indent indent = new Indent();
+        assertTrue(indent.right().left().toString().isEmpty());
+    }
+    
+}
