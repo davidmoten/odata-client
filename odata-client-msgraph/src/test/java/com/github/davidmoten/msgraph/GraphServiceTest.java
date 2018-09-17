42df4553c996346ce72b14ce5c0a6b085c8d9496
Dave Moten <davidmoten@gmail.com>
2018-09-18 07:03:40 +1000

    add get user test

--- odata-client-msgraph/src/test/java/com/github/davidmoten/msgraph/GraphServiceTest.java
+++ odata-client-msgraph/src/test/java/com/github/davidmoten/msgraph/GraphServiceTest.java
@@ -1,5 +1,9 @@
 package com.github.davidmoten.msgraph;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.Map;
 
 import org.junit.Test;
@@ -12,7 +16,7 @@ import com.github.davidmoten.odata.client.Serializer;
 import com.github.davidmoten.odata.client.Service;
 
 import odata.msgraph.client.container.GraphService;
-import odata.msgraph.client.entity.Contact;
+import odata.msgraph.client.entity.User;
 
 public class GraphServiceTest {
 
@@ -22,16 +26,27 @@ public class GraphServiceTest {
 
             @Override
             public ResponseGet getResponseGET(String url, Map<String, String> requestHeaders) {
-                return new ResponseGet(200, "{\"givenName\":\"fred\"}");
+                String text;
+                try {
+                    text = new String(
+                            Files.readAllBytes(
+                                    new File("src/test/resources/response-user.json").toPath()),
+                            StandardCharsets.UTF_8);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                return new ResponseGet(200, text);
             }
 
             @Override
             public Path getBasePath() {
-                return new Path("https://boo.com",PathStyle.IDENTIFIERS_AS_SEGMENTS);
+                return new Path("https://boo.com", PathStyle.IDENTIFIERS_AS_SEGMENTS);
 
-            }};
-        Serializer serializer = new Serializer() {};
+            }
+        };
+        Serializer serializer = new Serializer() {
+        };
         Context c = new Context(serializer, service);
-        Contact contact = new GraphService(c).users("1").contacts("2").get();
+        User user = new GraphService(c).users("1").get();
     }
 }
