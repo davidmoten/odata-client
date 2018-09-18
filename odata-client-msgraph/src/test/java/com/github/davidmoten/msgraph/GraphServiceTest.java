ba11ec3d0b6a14b4d96de3ba59b693336debfceb
Dave Moten <davidmoten@gmail.com>
2018-09-19 09:17:09 +1000

    add tests

--- odata-client-msgraph/src/test/java/com/github/davidmoten/msgraph/GraphServiceTest.java
+++ odata-client-msgraph/src/test/java/com/github/davidmoten/msgraph/GraphServiceTest.java
@@ -3,6 +3,7 @@ package com.github.davidmoten.msgraph;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -12,6 +13,7 @@ import com.github.davidmoten.odata.client.PathStyle;
 import com.github.davidmoten.odata.client.Serializer;
 import com.github.davidmoten.odata.client.Service;
 import com.github.davidmoten.odata.client.TestingService;
+import com.github.davidmoten.odata.client.TestingService.Builder;
 
 import odata.msgraph.client.container.GraphService;
 import odata.msgraph.client.entity.User;
@@ -37,11 +39,29 @@ public class GraphServiceTest {
         assertFalse(c.nextPage().isPresent());
     }
 
-    private static GraphService createClient(String path, String resource) {
-        Service service = TestingService //
+    @Test
+    public void testGetEntityCollectionWithNextPage() {
+        GraphService client = client(serviceBuilder().replyWithResource("/me/contacts", "/response-contacts.json"));
+        assertNotNull(client.me().contacts().get());
+        CollectionPageEntity<User> c = client.users().get();
+        assertNotNull(c);
+        assertEquals(10, c.currentPage().size());
+        assertTrue(c.nextPage().isPresent());
+    }
+
+    private GraphService client(Builder b) {
+        return new GraphService(new Context(Serializer.DEFAULT, b.build()));
+    }
+
+    private static TestingService.Builder serviceBuilder() {
+        return TestingService //
                 .baseUrl("https://graph.microsoft.com/v1.0") //
-                .pathStyle(PathStyle.IDENTIFIERS_AS_SEGMENTS) //
-                .replyWithResource("https://graph.microsoft.com/v1.0" + path, resource) //
+                .pathStyle(PathStyle.IDENTIFIERS_AS_SEGMENTS);
+    }
+
+    private static GraphService createClient(String path, String resource) {
+        Service service = serviceBuilder() //
+                .replyWithResource(path, resource) //
                 .build();
         return new GraphService(new Context(Serializer.DEFAULT, service));
     }
