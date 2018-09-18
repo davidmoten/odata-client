9327f52944460fb4c9b23e4bbe8a2351e481b938
Dave Moten <davidmoten@gmail.com>
2018-09-18 14:17:38 +1000

    add nextLink property to constructor parameters

--- odata-client-core/src/main/java/com/github/davidmoten/odata/client/CollectionPageEntity.java
+++ odata-client-core/src/main/java/com/github/davidmoten/odata/client/CollectionPageEntity.java
@@ -11,14 +11,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.github.davidmoten.guavamini.annotations.VisibleForTesting;
 
-public class CollectionPage<T> {
+public class CollectionPageEntity<T> {
 
     private final Class<T> cls;
     private final List<T> list;
     private final Optional<String> nextLink;
     private final Context context;
 
-    public CollectionPage(Class<T> cls, List<T> list, Optional<String> nextLink, Context context) {
+    public CollectionPageEntity(Class<T> cls, List<T> list, Optional<String> nextLink, Context context) {
         this.cls = cls;
         this.list = list;
         this.nextLink = nextLink;
@@ -29,7 +29,7 @@ public class CollectionPage<T> {
         return list;
     }
 
-    public Optional<CollectionPage<T>> nextPage() {
+    public Optional<CollectionPageEntity<T>> nextPage() {
         if (nextLink.isPresent()) {
             // TODO add request headers used in initial call?
             ResponseGet response = context.service().getResponseGET(nextLink.get(), Collections.emptyMap());
@@ -43,7 +43,7 @@ public class CollectionPage<T> {
     }
 
     @VisibleForTesting
-    static <T> Optional<CollectionPage<T>> nextPage(String json, Class<T> cls, Context context) {
+    static <T> Optional<CollectionPageEntity<T>> nextPage(String json, Class<T> cls, Context context) {
         try {
             ObjectMapper m = Serialization.MAPPER;
             ObjectNode o = m.readValue(json, ObjectNode.class);
@@ -52,13 +52,13 @@ public class CollectionPage<T> {
                 list2.add(context.serializer().deserialize(m.writeValueAsString(item), cls, null));
             }
             Optional<String> nextLink2 = Optional.ofNullable(o.get("@odata.nextLink").asText());
-            return Optional.ofNullable(new CollectionPage<T>(cls, list2, nextLink2, context));
+            return Optional.ofNullable(new CollectionPageEntity<T>(cls, list2, nextLink2, context));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
     
-    public static <T> CollectionPage<T> from(Context context, CollectionPageJson c, Class<T> cls) {
+    public static <T> CollectionPageEntity<T> from(Context context, CollectionPageJson c, Class<T> cls) {
         //TODO
         return null;
     }
--- odata-client-core/src/main/java/com/github/davidmoten/odata/client/CollectionPageNonEntity.java
+++ odata-client-core/src/main/java/com/github/davidmoten/odata/client/CollectionPageNonEntity.java
@@ -1,5 +1,33 @@
 package com.github.davidmoten.odata.client;
 
+import java.util.List;
+import java.util.Optional;
+
 public final class CollectionPageNonEntity<T> {
+
+    private final ContextPath contextPath;
+    private final Class<T> cls;
+    private final List<T> list;
+    private final String nextLink;
+
+    public CollectionPageNonEntity(ContextPath contextPath, Class<T> cls, List<T> list, String nextLink) {
+        this.contextPath = contextPath;
+        this.cls = cls;
+        this.list = list;
+        this.nextLink = nextLink;
+    }
+
+    public List<T> values() {
+        return list;
+    }
+
+    public Optional<CollectionPageNonEntity<T>> nextPage() {
+        if (nextLink != null) {
+            // TODO
+            return null;
+        } else {
+            return Optional.empty();
+        }
+    }
 
 }
