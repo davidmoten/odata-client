38b79f5c52af68c9f6f6366bcfc8e39ac007f5d8
Dave Moten <davidmoten@gmail.com>
2018-09-19 15:03:20 +1000

    add support for Java 8 time types

--- odata-client-core/src/main/java/com/github/davidmoten/odata/client/Serialization.java
+++ odata-client-core/src/main/java/com/github/davidmoten/odata/client/Serialization.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonInclude.Include;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public final class Serialization {
 
@@ -16,6 +17,7 @@ public final class Serialization {
     public static ObjectMapper createObjectMapper() {
         return new ObjectMapper() //
                 .registerModule(new Jdk8Module()) //
+                .registerModule(new JavaTimeModule())
                 .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false) //
                 .setSerializationInclusion(Include.NON_NULL);
     }
