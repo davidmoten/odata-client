c0bac6d601a54a5e8de5e310f43324ec10852ffa
Dave Moten <davidmoten@gmail.com>
2018-09-21 07:02:12 +1000

    add odata test service metadata as generation test

--- odata-client-core/src/test/java/com/github/davidmoten/odata/client/generator/GeneratorTest.java
+++ odata-client-core/src/test/java/com/github/davidmoten/odata/client/generator/GeneratorTest.java
@@ -15,10 +15,26 @@ public class GeneratorTest {
     public void testGenerateMsgraph() throws JAXBException {
         JAXBContext c = JAXBContext.newInstance(TDataServices.class);
         Unmarshaller unmarshaller = c.createUnmarshaller();
-        TEdmx t = unmarshaller.unmarshal(
-                new StreamSource(GeneratorTest.class.getResourceAsStream("/msgraph-1.0-20180905-formatted.xml")),
-                TEdmx.class).getValue();
+        TEdmx t = unmarshaller
+                .unmarshal(
+                        new StreamSource(GeneratorTest.class
+                                .getResourceAsStream("/msgraph-1.0-20180905-formatted.xml")),
+                        TEdmx.class)
+                .getValue();
         Generator g = new Generator(Options.builder().pkg("microsoft.graph.generated").build(),
+                t.getDataServices().getSchema().get(0));
+        g.generate();
+    }
+
+    @Test
+    public void testGenerateODataTestService() throws JAXBException {
+        JAXBContext c = JAXBContext.newInstance(TDataServices.class);
+        Unmarshaller unmarshaller = c.createUnmarshaller();
+        TEdmx t = unmarshaller.unmarshal(
+                new StreamSource(GeneratorTest.class
+                        .getResourceAsStream("/services.odata.org.v3.svc.metadata.xml")),
+                TEdmx.class).getValue();
+        Generator g = new Generator(Options.builder().pkg("odata.test.generated").build(),
                 t.getDataServices().getSchema().get(0));
         g.generate();
     }
