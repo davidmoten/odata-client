8f64e7b967879d22317399ac131d87a04b19896b
Dave Moten <davidmoten@gmail.com>
2018-09-21 19:18:13 +1000

    a little work on Edm.GeographyPoint

--- odata-client-runtime/src/main/java/com/github/davidmoten/odata/client/edm/GeographyPoint.java
+++ odata-client-runtime/src/main/java/com/github/davidmoten/odata/client/edm/GeographyPoint.java
@@ -5,16 +5,17 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 public final class GeographyPoint {
 
-    @JsonProperty("values")
-    private final double[] values;
+    @JsonProperty("type")
+    private final String type;
+
+    @JsonProperty("coordinates")
+    private final double[] coordinates;
 
     @JsonCreator()
-    public GeographyPoint(@JsonProperty("values") double[] values) {
-        this.values = values;
-    }
-
-    public double[] values() {
-        return values;
+    public GeographyPoint(@JsonProperty("type") String type,
+            @JsonProperty("coordinates") double[] coordinates) {
+        this.type = type;
+        this.coordinates = coordinates;
     }
 
 }
