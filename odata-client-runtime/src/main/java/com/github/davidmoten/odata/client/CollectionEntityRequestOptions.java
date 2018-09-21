9aaded2e97d4aca274ce4d2e73d569cbf3a2b207
Dave Moten <davidmoten@gmail.com>
2018-09-21 19:34:08 +1000

    query dev and tests

--- odata-client-runtime/src/main/java/com/github/davidmoten/odata/client/CollectionEntityRequestOptions.java
+++ odata-client-runtime/src/main/java/com/github/davidmoten/odata/client/CollectionEntityRequestOptions.java
@@ -12,15 +12,18 @@ public final class CollectionEntityRequestOptions implements RequestOptions {
     private final Optional<String> orderBy;
     private final Optional<Long> skip;
     private final Optional<Long> top;
+    private final Optional<String> select;
 
-    public CollectionEntityRequestOptions(Map<String, String> requestHeaders, Optional<String> search,
-            Optional<String> filter, Optional<String> orderBy, Optional<Long> skip, Optional<Long> top) {
+    public CollectionEntityRequestOptions(Map<String, String> requestHeaders,
+            Optional<String> search, Optional<String> filter, Optional<String> orderBy,
+            Optional<Long> skip, Optional<Long> top, Optional<String> select) {
         this.requestHeaders = requestHeaders;
         this.search = search;
         this.filter = filter;
         this.orderBy = orderBy;
         this.skip = skip;
         this.top = top;
+        this.select = select;
     }
 
     @Override
@@ -36,6 +39,7 @@ public final class CollectionEntityRequestOptions implements RequestOptions {
         orderBy.ifPresent(x -> map.put("$orderBy", x));
         skip.ifPresent(x -> map.put("$skip", String.valueOf(x)));
         top.ifPresent(x -> map.put("$top", String.valueOf(x)));
+        select.ifPresent(x -> map.put("$select", x));
         return map;
     }
 
