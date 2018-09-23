e776503f7d91dfead5d76aa8cdb2f18a5f409bdb
Dave Moten <davidmoten@gmail.com>
2018-09-23 13:37:11 +1000

    change Structure to abstract class and implement getHeirarchy there

--- odata-client-generator/src/main/java/com/github/davidmoten/odata/client/generator/Structure.java
+++ odata-client-generator/src/main/java/com/github/davidmoten/odata/client/generator/Structure.java
@@ -1,22 +1,52 @@
 package com.github.davidmoten.odata.client.generator;
 
+import java.util.LinkedList;
 import java.util.List;
+import java.util.function.Function;
 
 import org.oasisopen.odata.csdl.v4.TNavigationProperty;
 import org.oasisopen.odata.csdl.v4.TProperty;
 
-public interface Structure<T> {
+public abstract class Structure<T> {
 
-    String getName();
+    protected final T value;
+    private final Class<T> cls;
+    protected final Names names;
+    private final Function<T, Structure<T>> factory;
 
-    String getBaseType();
+    public Structure(T value, Class<T> cls, Names names, Function<T, Structure<T>> factory) {
+        this.value = value;
+        this.cls = cls;
+        this.names = names;
+        this.factory = factory;
+    }
 
-    T value();
+    abstract String getName();
 
-    List<TProperty> getProperties();
+    abstract String getBaseType();
 
-    List<TNavigationProperty> getNavigationProperties();
+    abstract List<TProperty> getProperties();
 
-    List<T> getHeirarchy();
+    abstract List<TNavigationProperty> getNavigationProperties();
+    
+    public final List<T> getHeirarchy() {
+        List<T> a = new LinkedList<>();
+        a.add(value);
+        Structure<T> st = this;
+        while (true) {
+            if (st.getBaseType() == null) {
+                return a;
+            } else {
+                String baseTypeSimpleName = names
+                        .getSimpleTypeNameFromTypeWithNamespace(st.getBaseType());
+                st = Util.types(names.getSchema(), cls) //
+                        .map(factory) //
+                        .filter(x -> x.getName().equals(baseTypeSimpleName)) //
+                        .findFirst() //
+                        .get();
+                a.add(0, st.value);
+            }
+        }
+    }
 
 }
