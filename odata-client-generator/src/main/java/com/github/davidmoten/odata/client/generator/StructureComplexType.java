824678dc5d84bff688e9a18416a383fdf689261d
Dave Moten <davidmoten@gmail.com>
2018-09-23 13:37:11 +1000

    change Structure to abstract class and implement getHeirarchy there

--- odata-client-generator/src/main/java/com/github/davidmoten/odata/client/generator/StructureComplexType.java
+++ odata-client-generator/src/main/java/com/github/davidmoten/odata/client/generator/StructureComplexType.java
@@ -1,68 +1,37 @@
 package com.github.davidmoten.odata.client.generator;
 
-import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.oasisopen.odata.csdl.v4.TComplexType;
 import org.oasisopen.odata.csdl.v4.TNavigationProperty;
 import org.oasisopen.odata.csdl.v4.TProperty;
 
-public class StructureComplexType implements Structure<TComplexType> {
-
-    private final TComplexType c;
-    private final Names names;
+public final class StructureComplexType extends Structure<TComplexType> {
 
     public StructureComplexType(TComplexType c, Names names) {
-        this.c = c;
-        this.names = names;
+        super(c, TComplexType.class, names, t -> new StructureComplexType(t, names));
     }
 
     @Override
     public String getName() {
-        return c.getName();
+        return value.getName();
     }
 
     @Override
     public String getBaseType() {
-        return c.getBaseType();
-    }
-
-    @Override
-    public TComplexType value() {
-        return c;
+        return value.getBaseType();
     }
 
     @Override
     public List<TProperty> getProperties() {
-        return Util.filter(c.getPropertyOrNavigationPropertyOrAnnotation(), TProperty.class)
+        return Util.filter(value.getPropertyOrNavigationPropertyOrAnnotation(), TProperty.class)
                 .collect(Collectors.toList());
     }
 
     @Override
-    public List<TComplexType> getHeirarchy() {
-        TComplexType t = value();
-        List<TComplexType> a = new LinkedList<>();
-        a.add(t);
-        while (true) {
-            if (t.getBaseType() == null) {
-                return a;
-            } else {
-                TComplexType y = t;
-                t = Util.types(names.getSchema(), TComplexType.class) //
-                        .filter(x -> x.getName().equals(
-                                names.getSimpleTypeNameFromTypeWithNamespace(y.getBaseType()))) //
-                        .findFirst() //
-                        .get();
-                a.add(0, t);
-            }
-        }
-    }
-
-    @Override
     public List<TNavigationProperty> getNavigationProperties() {
-        return Util.filter(value().getPropertyOrNavigationPropertyOrAnnotation(),
+        return Util.filter(value.getPropertyOrNavigationPropertyOrAnnotation(),
                 TNavigationProperty.class).collect(Collectors.toList());
     }
 }
--- odata-client-generator/src/main/java/com/github/davidmoten/odata/client/generator/StructureEntityType.java
+++ odata-client-generator/src/main/java/com/github/davidmoten/odata/client/generator/StructureEntityType.java
@@ -1,70 +1,37 @@
 package com.github.davidmoten.odata.client.generator;
 
-import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.oasisopen.odata.csdl.v4.TEntityType;
 import org.oasisopen.odata.csdl.v4.TNavigationProperty;
 import org.oasisopen.odata.csdl.v4.TProperty;
 
-public class StructureEntityType implements Structure<TEntityType> {
-
-    private final TEntityType c;
-    private final Names names;
+public final class StructureEntityType extends Structure<TEntityType> {
 
     public StructureEntityType(TEntityType c, Names names) {
-        this.c = c;
-        this.names = names;
+        super(c, TEntityType.class, names, t -> new StructureEntityType(t, names));
     }
 
     @Override
     public String getName() {
-        return c.getName();
+        return value.getName();
     }
 
     @Override
     public String getBaseType() {
-        return c.getBaseType();
-    }
-
-    @Override
-    public TEntityType value() {
-        return c;
+        return value.getBaseType();
     }
 
     @Override
     public List<TProperty> getProperties() {
-        return Util.filter(c.getKeyOrPropertyOrNavigationProperty(), TProperty.class)
+        return Util.filter(value.getKeyOrPropertyOrNavigationProperty(), TProperty.class)
                 .collect(Collectors.toList());
     }
 
     @Override
     public List<TNavigationProperty> getNavigationProperties() {
-        return Util
-                .filter(value().getKeyOrPropertyOrNavigationProperty(), TNavigationProperty.class)
-                .collect(Collectors.toList());
+        return Util.filter(value.getKeyOrPropertyOrNavigationProperty(),
+                TNavigationProperty.class).collect(Collectors.toList());
     }
-
-    @Override
-    public List<TEntityType> getHeirarchy() {
-        List<TEntityType> a = new LinkedList<>();
-        TEntityType t = value();
-        a.add(t);
-        while (true) {
-            if (t.getBaseType() == null) {
-                return a;
-            } else {
-                TEntityType y = t;
-                t = Util.types(names.getSchema(), TEntityType.class) //
-                        .filter(x -> x.getName().equals(
-                                names.getSimpleTypeNameFromTypeWithNamespace(y.getBaseType()))) //
-                        .findFirst() //
-                        .get();
-                a.add(0, t);
-            }
-        }
-    }
-
 }
