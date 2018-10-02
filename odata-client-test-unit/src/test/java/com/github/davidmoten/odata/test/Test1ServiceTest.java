a44d162175d46548da81a4e361ffc3957afbd865
Dave Moten <davidmoten@gmail.com>
2018-10-02 15:41:23 +1000

    add unit tests for generation

--- odata-client-test-unit/src/test/java/com/github/davidmoten/odata/test/Test1ServiceTest.java
+++ odata-client-test-unit/src/test/java/com/github/davidmoten/odata/test/Test1ServiceTest.java
@@ -2,13 +2,13 @@ package com.github.davidmoten.odata.test;
 
 import org.junit.Test;
 
-import test1.container.Test1Service;
+import test1b.container.Test1Service;
 
 public class Test1ServiceTest {
 
     @Test
-    public void testTopLevelCollection() {
-         Test1Service client = Test1Service.test().baseUrl("http://base").build();
+    public void testCanReferenceEntityInAnotherSchema() {
+          Test1Service client = Test1Service.test().baseUrl("http://base").build();
          client.products("1");
     }
 
