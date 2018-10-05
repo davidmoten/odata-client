2c325f828de241849b89935c149be2e169083557
Dave Moten <davidmoten@gmail.com>
2018-10-05 17:04:45 +1000

    add UnmappedFields class

---
+++ odata-client-runtime/src/main/java/com/github/davidmoten/odata/client/internal/UnmappedFields.java
@@ -0,0 +1,11 @@
+package com.github.davidmoten.odata.client.internal;
+
+import java.util.HashMap;
+
+public final class UnmappedFields extends HashMap<String, Object> {
+
+    private static final long serialVersionUID = 823105277916737973L;
+
+    public static final UnmappedFields EMPTY = new UnmappedFields();
+
+}
