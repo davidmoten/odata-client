22b49d36d2ade27cf5ec6642bce9ce0ab099a0f3
Dave Moten <davidmoten@gmail.com>
2018-10-08 21:04:24 +1100

    change test to patch with minimal metadata

--- odata-client-generator/src/main/java/com/github/davidmoten/odata/client/generator/Generator.java
+++ odata-client-generator/src/main/java/com/github/davidmoten/odata/client/generator/Generator.java
@@ -11,7 +11,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import org.oasisopen.odata.csdl.v4.Schema;
