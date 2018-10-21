c9b90ac17c697f299e702720a8fc00024908f58a
Dave Moten <davidmoten@gmail.com>
2018-10-22 08:28:11 +1100

    Edm.Stream support dev

---
+++ odata-client-runtime/src/main/java/com/github/davidmoten/odata/client/StreamWithContentType.java
@@ -0,0 +1,32 @@
+package com.github.davidmoten.odata.client;
+
+import java.io.InputStream;
+import java.util.function.Supplier;
+
+import com.github.davidmoten.guavamini.Preconditions;
+import com.github.davidmoten.odata.client.internal.RequestHelper;
+
+public final class StreamWithContentType implements Supplier<InputStream> {
+
+    private final ContextPath contextPath;
+    private final RequestOptions options;
+    private final String contentType;
+
+    public StreamWithContentType(ContextPath contextPath, RequestOptions options, String contentType) {
+        Preconditions.checkNotNull(contextPath);
+        Preconditions.checkNotNull(contentType);
+        this.contextPath = contextPath;
+        this.options = options;
+        this.contentType = contentType;
+    }
+
+    @Override
+    public InputStream get() {
+        return RequestHelper.getStream(contextPath, options);
+    }
+
+    public String contentType() {
+        return contentType;
+    }
+
+}
