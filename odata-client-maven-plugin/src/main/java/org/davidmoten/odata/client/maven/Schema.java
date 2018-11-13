14d8ed18df64aee9b36bf4de08e4975edb33e1d5
Dave Moten <davidmoten@gmail.com>
2018-11-13 11:02:51 +1100

    change package path for generated entity collections

--- odata-client-maven-plugin/src/main/java/org/davidmoten/odata/client/maven/Schema.java
+++ odata-client-maven-plugin/src/main/java/org/davidmoten/odata/client/maven/Schema.java
@@ -9,7 +9,7 @@ public class Schema {
     public String packageSuffixEntity = ".entity";
     public String packageSuffixComplexType = ".complex";
     public String packageSuffixEntityRequest = ".entity.request";
-    public String packageSuffixCollectionRequest = ".collection.request";
+    public String packageSuffixCollectionRequest = ".entity.collection.request";
     public String packageSuffixContainer = ".container";
     public String packageSuffixSchema = ".schema";
     public String simpleClassNameSchema = "SchemaInfo";
