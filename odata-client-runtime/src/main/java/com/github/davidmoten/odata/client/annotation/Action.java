eb98baff5695b0292846baf6baece0fa877131c2
Dave Moten <davidmoten@gmail.com>
2020-02-12 21:05:18 +1100

    add Action annotation

---
+++ odata-client-runtime/src/main/java/com/github/davidmoten/odata/client/annotation/Action.java
@@ -0,0 +1,12 @@
+package com.github.davidmoten.odata.client.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Action {
+     String name();
+}
