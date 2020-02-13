37564835df1c4eb61f9058d29023708ee517fe92
David Moten <davidmoten@gmail.com>
2020-02-13 14:30:32 +1100

    add api test of action

--- odata-client-test-unit/src/test/java/com/github/davidmoten/odata/test/Test6ServiceTest.java
+++ odata-client-test-unit/src/test/java/com/github/davidmoten/odata/test/Test6ServiceTest.java
@@ -1,5 +1,24 @@
 package com.github.davidmoten.odata.test;
 
+import org.junit.Test;
+
+import com.github.davidmoten.guavamini.Lists;
+
+import test6.a.entity.Product;
+
 public class Test6ServiceTest {
+    
+    @SuppressWarnings("unused")
+    @Test
+    public void testActionApiCompiles() {
+        Product p = null;
+        if (false) {
+            String answer = p.relatedProducts(123, Lists.newArrayList(10,20,30)) //
+            .select("id") //
+            .expand("attachments") //
+            .metadataFull() //
+            .call();
+        }
+    }
 
 }
