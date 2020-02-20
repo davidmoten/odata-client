b95bdc60460d7dfd247e438174b5fd9dcf5bf4f3
David Moten <davidmoten@gmail.com>
2020-02-20 12:16:17 +1100

    add custom request to all clients

---
+++ odata-client-runtime/src/main/java/com/github/davidmoten/odata/client/HasContext.java
@@ -0,0 +1,11 @@
+package com.github.davidmoten.odata.client;
+
+public interface HasContext {
+
+    public Context _context();
+    
+    public default CustomRequest _custom() {
+        return new CustomRequest(_context());
+    }
+    
+}
