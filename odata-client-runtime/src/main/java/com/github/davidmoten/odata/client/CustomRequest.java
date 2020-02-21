c26621acb6d66305bd33458726fb389b2378e585
David Moten <davidmoten@gmail.com>
2020-02-21 12:24:58 +1100

    custom request dev

--- odata-client-runtime/src/main/java/com/github/davidmoten/odata/client/CustomRequest.java
+++ odata-client-runtime/src/main/java/com/github/davidmoten/odata/client/CustomRequest.java
@@ -20,11 +20,9 @@ public final class CustomRequest {
         this.context = context;
     }
 
-    public <T> T get(String url, Class<T> responseCls, RequestHeader... headers) {
+    public <T> T get(String url, Class<T> responseCls, SchemaInfo schemaInfo, RequestHeader... headers) {
         UrlInfo info = getInfo(context, url, headers);
-        //TODO set schemaInfo
-        RequestHelper.get(info.contextPath, responseCls, info, null);
-        throw new UnsupportedOperationException("not implemented yet");
+        return RequestHelper.get(info.contextPath, responseCls, info, schemaInfo);
     }
 
     public <T> String getJson(String url, RequestHeader... headers) {
@@ -54,23 +52,24 @@ public final class CustomRequest {
     }
 
     private static UrlInfo getInfo(Context context, String url, RequestHeader[] requestHeaders) {
-        try {
-            String s = URLEncoder.encode(url, "UTF-8");
-            String p;
-            int i = url.indexOf('?');
-            if (i == -1) {
-                p = s;
-            } else {
-                p = s.substring(0, i);
-            }
-            Path path = new Path(p, context.service().getBasePath().style());
-            ContextPath contextPath = new ContextPath(context, path);
-            Map<String, String> queries = URLEncodedUtils.parse(url, StandardCharsets.UTF_8) //
-                    .stream().collect(Collectors.toMap(pair -> pair.getName(), pair -> pair.getValue()));
-            return new UrlInfo(contextPath, queries, Arrays.asList(requestHeaders));
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException(e);
+        final String urlPath;
+        final String urlQuery;
+        int i = url.indexOf('?');
+        if (i == -1) {
+            urlPath = url;
+            urlQuery = "";
+        } else {
+            urlPath = url.substring(0, i);
+            urlQuery = url.substring(i, url.length());
         }
+        Path path = new Path(urlPath, context.service().getBasePath().style());
+        ContextPath contextPath = new ContextPath(context, path);
+        Map<String, String> queries = URLEncodedUtils //
+                .parse(urlQuery, StandardCharsets.UTF_8) //
+                .stream() //
+                .peek(x -> System.out.println("pair=" + x)) //
+                .collect(Collectors.toMap(pair -> pair.getName(), pair -> pair.getValue()));
+        return new UrlInfo(contextPath, queries, Arrays.asList(requestHeaders));
     }
 
     private static final class UrlInfo implements RequestOptions {
