d526d7d8b612c015c3fd48e0e459fcf22a818991
David Moten <davidmoten@gmail.com>
2020-02-27 15:39:45 +1100

    cross schema type references still failing in msgraph beta

--- odata-client-generator/src/test/java/com/github/davidmoten/odata/client/generator/GeneratorTest.java
+++ odata-client-generator/src/test/java/com/github/davidmoten/odata/client/generator/GeneratorTest.java
@@ -26,8 +26,7 @@ public class GeneratorTest {
         JAXBContext c = JAXBContext.newInstance(TDataServices.class);
         Unmarshaller unmarshaller = c.createUnmarshaller();
         TEdmx t = unmarshaller
-                .unmarshal(new StreamSource(new FileInputStream("src/main/odata/msgraph-metadata.xml")),
-                        TEdmx.class)
+                .unmarshal(new StreamSource(new FileInputStream("src/main/odata/msgraph-metadata.xml")), TEdmx.class)
                 .getValue();
         SchemaOptions schemaOptions = new SchemaOptions("microsoft.graph", "microsoft.graph.generated");
         Options options = new Options(GENERATED, Collections.singletonList(schemaOptions));
@@ -36,6 +35,20 @@ public class GeneratorTest {
         File file = new File(GENERATED + "/microsoft/graph/generated/entity/FileAttachment.java");
         Files.copy(file.toPath(), new File("../src/docs/FileAttachment.java").toPath(),
                 StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    @Test
+    public void testGenerateMsgraphBeta() throws JAXBException, IOException {
+        JAXBContext c = JAXBContext.newInstance(TDataServices.class);
+        Unmarshaller unmarshaller = c.createUnmarshaller();
+        TEdmx t = unmarshaller
+                .unmarshal(new StreamSource(new FileInputStream("../odata-client-msgraph-beta/src/main/odata/msgraph-beta-metadata.xml")), TEdmx.class)
+                .getValue();
+        
+        SchemaOptions schemaOptions = new SchemaOptions("microsoft.graph", "microsoft.graph.betas.generated");
+        Options options = new Options(GENERATED, Collections.singletonList(schemaOptions));
+        Generator g = new Generator(options, t.getDataServices().getSchema());
+        g.generate();
     }
 
     @Test
@@ -52,7 +65,6 @@ public class GeneratorTest {
         g.generate();
     }
 
-    
     public static void main(String[] args) throws JAXBException, IOException {
         while (true) {
             new GeneratorTest().testGenerateMsgraph();
