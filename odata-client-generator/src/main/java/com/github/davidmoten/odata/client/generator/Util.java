68614c0b4c6f50515c01de2622d4b281c1bd61cb
Dave Moten <davidmoten@gmail.com>
2020-03-02 19:26:09 +1100

    cleanup

--- odata-client-generator/src/main/java/com/github/davidmoten/odata/client/generator/Util.java
+++ odata-client-generator/src/main/java/com/github/davidmoten/odata/client/generator/Util.java
@@ -5,7 +5,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.oasisopen.odata.csdl.v4.Annotation;
 import org.oasisopen.odata.csdl.v4.Schema;
 import org.oasisopen.odata.csdl.v4.TAction;
 import org.oasisopen.odata.csdl.v4.TActionFunctionParameter;
