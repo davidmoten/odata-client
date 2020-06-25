c57cdec1cd73dbf840cd3bf857b0dbd6e67b9554
Dave Moten <davidmoten@gmail.com>
2020-06-25 22:02:17 +1000

    change MsGraph.explorer() to return builder so can set proxy

--- odata-client-msgraph/src/main/java/com/github/davidmoten/msgraph/MsGraph.java
+++ odata-client-msgraph/src/main/java/com/github/davidmoten/msgraph/MsGraph.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import com.github.davidmoten.msgraph.builder.GraphExplorerHttpService;
 import com.github.davidmoten.msgraph.builder.MsGraphClientBuilder;
+import com.github.davidmoten.msgraph.builder.MsGraphClientBuilder.Builder3;
 
 import odata.msgraph.client.container.GraphService;
 
@@ -21,14 +22,13 @@ public final class MsGraph {
                 context -> new GraphService(context)).tenantName(tenantName);
     }
 
-    public static GraphService explorer() {
+    public static Builder3<GraphService> explorer() {
         return MsGraph //
                 .tenantName("unused") //
                 .clientId("unused") //
                 .clientSecret("unused") //
                 .refreshBeforeExpiry(5, TimeUnit.MINUTES) //
                 .accessTokenProvider(() -> "{token:https://graph.microsoft.com/}") //
-                .httpServiceTransformer(s -> new GraphExplorerHttpService(s)) //
-                .build();
+                .httpServiceTransformer(s -> new GraphExplorerHttpService(s));
     }
 }
