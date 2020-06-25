c24c0e1e661aec7547da0ac8bd98f54a4f1d29e8
David Moten <davidmoten@gmail.com>
2020-06-26 09:11:49 +1000

    add GraphExplorer support to beta client

---
+++ odata-client-msgraph-beta/src/test/java/com/github/davidmoten/msgraph/beta/GraphExplorerMain.java
@@ -0,0 +1,12 @@
+package com.github.davidmoten.msgraph.beta;
+
+import odata.msgraph.client.beta.container.GraphService;
+
+public class GraphExplorerMain {
+    
+    public static void main(String[] args) {
+        GraphService client = MsGraph.explorer().build();
+        client.applications().stream().forEach(System.out::println);
+    }
+
+}
