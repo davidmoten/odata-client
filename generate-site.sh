51ae8d63bf306a3e7fbab603f0b4efbbe1da5875
Dave Moten <davidmoten@gmail.com>
2020-06-26 23:11:27 +1000

    update generate-site.sh

--- generate-site.sh
+++ generate-site.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
-set -e
 PROJECT=odata-client
+set -e
+mvn clean install 
 mvn site
+mvn site:stage -Dsite.url=file://`pwd`/target/staging-temp
 cd ../davidmoten.github.io
 git pull
 mkdir -p $PROJECT
-cp -r ../$PROJECT/target/site/* $PROJECT/
+cp -r ../$PROJECT/target/staging/* $PROJECT/
 git add .
-git commit -am "update $PROJECT site reports"
+git commit -am "update site reports for $PROJECT"
 git push
+
