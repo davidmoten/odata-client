ac0867aa209c9c0ff0c951f2087d3edf25d52741
David Moten <davidmoten@gmail.com>
2020-07-21 15:15:07 +1000

    add main test

--- odata-client-msgraph/src/test/java/com/github/davidmoten/msgraph/MsGraphMain.java
+++ odata-client-msgraph/src/test/java/com/github/davidmoten/msgraph/MsGraphMain.java
@@ -1,14 +1,17 @@
 package com.github.davidmoten.msgraph;
 
+import java.util.Date;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
+import odata.msgraph.client.complex.AttachmentItem;
 import odata.msgraph.client.complex.EmailAddress;
 import odata.msgraph.client.complex.ItemBody;
 import odata.msgraph.client.complex.Recipient;
 import odata.msgraph.client.container.GraphService;
 import odata.msgraph.client.entity.Message;
 import odata.msgraph.client.entity.request.MailFolderRequest;
+import odata.msgraph.client.enums.AttachmentType;
 import odata.msgraph.client.enums.BodyType;
 
 public class MsGraphMain {
@@ -21,101 +24,127 @@ public class MsGraphMain {
                 .clientSecret(System.getProperty("clientSecret")) //
                 .refreshBeforeExpiry(5, TimeUnit.MINUTES) //
                 .build();
-        
-        client.users().select("userPrincipalName").stream().forEach(user -> System.out.println(user.getUserPrincipalName()));
+        {
+            String mailbox = System.getProperty("mailbox");
+            MailFolderRequest drafts = client //
+                    .users(mailbox) //
+                    .mailFolders("Drafts");
+            Message m = Message.builderMessage() //
+                    .subject("hi there " + new Date()) //
+                    .body(ItemBody.builder() //
+                            .content("hello there how are you") //
+                            .contentType(BodyType.TEXT).build()) //
+                    .from(Recipient.builder() //
+                            .emailAddress(EmailAddress.builder() //
+                                    .address(mailbox) //
+                                    .build())
+                            .build())
+                    .build();
+            m = drafts.messages().post(m);
+            AttachmentItem a = AttachmentItem.builder().attachmentType(AttachmentType.FILE).contentType("text/plain")
+                    .name("attachment.txt").size(50L).build();
+            String url = client.users(mailbox).messages(m.getId().get()).attachments().createUploadSession(a).get()
+                    .value().getUploadUrl().get();
+            System.out.println(url);
+        }
         
         System.exit(0);
+        client.users().select("userPrincipalName").stream()
+                .forEach(user -> System.out.println(user.getUserPrincipalName()));
 
-        String mailbox = System.getProperty("mailbox");
-        MailFolderRequest drafts = client //
-                .users(mailbox) //
-                .mailFolders("Drafts");
+        System.exit(0);
+        {
+            String mailbox = System.getProperty("mailbox");
+            MailFolderRequest drafts = client //
+                    .users(mailbox) //
+                    .mailFolders("Drafts");
 
-        // test streaming of DriveItem.content
-//        String user = System.getProperty("mailbox");
-//        client.users(user).drive().items("01N6X7VZ6TXOGWV354WND3QHDNUYRXKVVH").get();
-//        for (DriveItem item : client //
-//                .users(user) //
-//                .drive() //
-//                .root() //
-//                .children() //
-//                .id("Attachments") //
-//                .children() //
-//                .metadataFull() //
-//                .get()) {
-//            // read content and count bytes
-//            StreamProvider stream = item.getContent().get();
-//            item.getUnmappedFields().entrySet().forEach(System.out::println);
-//            System.out.println(stream.contentType());
-//            byte[] bytes = toBytes(stream);
-//            System.out.println("read " + item.getName().orElse("?") + " size=" + bytes.length);
-//        }
+            // test streaming of DriveItem.content
+            // String user = System.getProperty("mailbox");
+            // client.users(user).drive().items("01N6X7VZ6TXOGWV354WND3QHDNUYRXKVVH").get();
+            // for (DriveItem item : client //
+            // .users(user) //
+            // .drive() //
+            // .root() //
+            // .children() //
+            // .id("Attachments") //
+            // .children() //
+            // .metadataFull() //
+            // .get()) {
+            // // read content and count bytes
+            // StreamProvider stream = item.getContent().get();
+            // item.getUnmappedFields().entrySet().forEach(System.out::println);
+            // System.out.println(stream.contentType());
+            // byte[] bytes = toBytes(stream);
+            // System.out.println("read " + item.getName().orElse("?") + " size=" +
+            // bytes.length);
+            // }
 
-        // this system integration test
-        // counts the messages in Drafts folder
-        // adds a new message to the Drafts folder
-        // changes the subject of the new message
-        // checks that the count of messages has increased by one
-        // deletes the message
-        
-        // count number of messages in Drafts
-        long count = drafts.messages() //
-                .metadataNone() //
-                .get() //
-                .stream() //
-                .count(); //
+            // this system integration test
+            // counts the messages in Drafts folder
+            // adds a new message to the Drafts folder
+            // changes the subject of the new message
+            // checks that the count of messages has increased by one
+            // deletes the message
 
-        // Prepare a new message
-        String id = UUID.randomUUID().toString().substring(0, 6);
-        Message m = Message.builderMessage() //
-                .subject("hi there " + id) //
-                .body(ItemBody.builder() //
-                        .content("hello there how are you") //
-                        .contentType(BodyType.TEXT).build()) //
-                .from(Recipient.builder() //
-                        .emailAddress(EmailAddress.builder() //
-                                .address(mailbox) //
-                                .build())
-                        .build())
-                .build();
+            // count number of messages in Drafts
+            long count = drafts.messages() //
+                    .metadataNone() //
+                    .get() //
+                    .stream() //
+                    .count(); //
 
-        // Create the draft message
-        Message saved = drafts.messages().post(m);
-        System.out.println("saved=" + saved);
+            // Prepare a new message
+            String id = UUID.randomUUID().toString().substring(0, 6);
+            Message m = Message.builderMessage() //
+                    .subject("hi there " + id) //
+                    .body(ItemBody.builder() //
+                            .content("hello there how are you") //
+                            .contentType(BodyType.TEXT).build()) //
+                    .from(Recipient.builder() //
+                            .emailAddress(EmailAddress.builder() //
+                                    .address(mailbox) //
+                                    .build())
+                            .build())
+                    .build();
 
-        // change subject
-        // Would have been nice to do like this but patch from the 
-        // path associated with saved is not supported.
-        // saved.withSubject("new subject " + id).patch();
-        client //
-                .users(mailbox) //
-                .messages(saved.getId().get()) //
-                .patch(saved.withSubject("new subject " + id));
+            // Create the draft message
+            Message saved = drafts.messages().post(m);
+            System.out.println("saved=" + saved);
 
-        // check the subject has changed by reloading the message
-        String amendedSubject = drafts //
-                .messages(saved.getId().get()) //
-                .get() //
-                .getSubject() //
-                .get();
-        if (!("new subject " + id).equals(amendedSubject)) {
-            throw new RuntimeException("subject not amended");
+            // change subject
+            // Would have been nice to do like this but patch from the
+            // path associated with saved is not supported.
+            // saved.withSubject("new subject " + id).patch();
+            client //
+                    .users(mailbox) //
+                    .messages(saved.getId().get()) //
+                    .patch(saved.withSubject("new subject " + id));
+
+            // check the subject has changed by reloading the message
+            String amendedSubject = drafts //
+                    .messages(saved.getId().get()) //
+                    .get() //
+                    .getSubject() //
+                    .get();
+            if (!("new subject " + id).equals(amendedSubject)) {
+                throw new RuntimeException("subject not amended");
+            }
+
+            long count2 = drafts //
+                    .messages() //
+                    .metadataNone() //
+                    .get() //
+                    .stream() //
+                    .count(); //
+            if (count2 != count + 1) {
+                throw new RuntimeException("unexpected count");
+            }
+
+            // Delete the draft message
+            drafts //
+                    .messages(saved.getId().get()) //
+                    .delete();
         }
-
-        long count2 = drafts //
-                .messages() //
-                .metadataNone() //
-                .get() //
-                .stream() //
-                .count(); //
-        if (count2 != count + 1) {
-            throw new RuntimeException("unexpected count");
-        }
-
-        // Delete the draft message
-        drafts //
-                .messages(saved.getId().get()) //
-                .delete();
-
     }
 }
