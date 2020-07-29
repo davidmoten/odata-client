846a108f4fe6eb54ef461bd5f6097f150ff0f039
David Moten <davidmoten@gmail.com>
2020-07-29 12:29:35 +1000

    format InjectableValuesFromFactories

--- odata-client-runtime/src/main/java/com/github/davidmoten/odata/client/internal/InjectableValuesFromFactories.java
+++ odata-client-runtime/src/main/java/com/github/davidmoten/odata/client/internal/InjectableValuesFromFactories.java
@@ -10,53 +10,50 @@ import com.fasterxml.jackson.databind.InjectableValues;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.util.ClassUtil;
 
-public final class InjectableValuesFromFactories
-        extends InjectableValues
-        implements java.io.Serializable
-    {
-        private static final long serialVersionUID = 1L;
+public final class InjectableValuesFromFactories extends InjectableValues implements java.io.Serializable {
 
-        protected final Map<String,Callable<?>> _values;
-        
-        public InjectableValuesFromFactories() {
-            this(new HashMap<String,Callable<?>>());
-        }
+    private static final long serialVersionUID = 6050577234392343535L;
 
-        public InjectableValuesFromFactories(Map<String, Callable<?>> values) {
-            _values = values;
-        }
+    protected final Map<String, Callable<?>> _values;
 
-        public InjectableValuesFromFactories addValue(String key, Callable<?> value) {
-            _values.put(key, value);
-            return this;
-        }
-
-        public InjectableValuesFromFactories addValue(Class<?> classKey, Callable<?> value) {
-            _values.put(classKey.getName(), value);
-            return this;
-        }
-        
-        @Override
-        public Object findInjectableValue(Object valueId, DeserializationContext ctxt,
-                BeanProperty forProperty, Object beanInstance) throws JsonMappingException
-        {
-            if (!(valueId instanceof String)) {
-                ctxt.reportBadDefinition(ClassUtil.classOf(valueId),
-                        String.format(
-                        "Unrecognized inject value id type (%s), expecting String",
-                        ClassUtil.classNameOf(valueId)));
-            }
-            String key = (String) valueId;
-            Callable<?> callable = _values.get(key);
-            Object ob;
-            try {
-                ob = callable.call();
-            } catch (Exception e) {
-                throw JsonMappingException.from(ctxt,"callable threw when creating value for property " + forProperty.getFullName(), e);
-            }
-            if (ob == null && !_values.containsKey(key)) {
-                throw new IllegalArgumentException("No injectable id with value '"+key+"' found (for property '"+forProperty.getName()+"')");
-            }
-            return ob;
-        }
+    public InjectableValuesFromFactories() {
+        this(new HashMap<String, Callable<?>>());
     }
+
+    public InjectableValuesFromFactories(Map<String, Callable<?>> values) {
+        _values = values;
+    }
+
+    public InjectableValuesFromFactories addValue(String key, Callable<?> value) {
+        _values.put(key, value);
+        return this;
+    }
+
+    public InjectableValuesFromFactories addValue(Class<?> classKey, Callable<?> value) {
+        _values.put(classKey.getName(), value);
+        return this;
+    }
+
+    @Override
+    public Object findInjectableValue(Object valueId, DeserializationContext ctxt, BeanProperty forProperty,
+            Object beanInstance) throws JsonMappingException {
+        if (!(valueId instanceof String)) {
+            ctxt.reportBadDefinition(ClassUtil.classOf(valueId), String.format(
+                    "Unrecognized inject value id type (%s), expecting String", ClassUtil.classNameOf(valueId)));
+        }
+        String key = (String) valueId;
+        Callable<?> callable = _values.get(key);
+        Object ob;
+        try {
+            ob = callable.call();
+        } catch (Exception e) {
+            throw JsonMappingException.from(ctxt,
+                    "callable threw when creating value for property " + forProperty.getFullName(), e);
+        }
+        if (ob == null && !_values.containsKey(key)) {
+            throw new IllegalArgumentException(
+                    "No injectable id with value '" + key + "' found (for property '" + forProperty.getName() + "')");
+        }
+        return ob;
+    }
+}
