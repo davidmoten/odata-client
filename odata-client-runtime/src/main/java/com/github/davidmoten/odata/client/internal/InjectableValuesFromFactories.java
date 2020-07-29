d8cbe44343b4a4088a3adaf63d876a9f58ca0289
Dave Moten <davidmoten@gmail.com>
2020-07-30 07:52:05 +1000

    Use injectable factories so that UnmappedValues and ChangedFields are initialized properly in deserialized lists (#36)
    * serialization of lists requires separate instances of ChangedFields and UnmappedFields so use factories in InjectableValues object, inspired by #35

---
+++ odata-client-runtime/src/main/java/com/github/davidmoten/odata/client/internal/InjectableValuesFromFactories.java
@@ -0,0 +1,68 @@
+package com.github.davidmoten.odata.client.internal;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.Callable;
+
+import com.fasterxml.jackson.databind.BeanProperty;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.InjectableValues;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.util.ClassUtil;
+import com.github.davidmoten.guavamini.Preconditions;
+
+public final class InjectableValuesFromFactories extends InjectableValues implements java.io.Serializable {
+
+    private static final long serialVersionUID = 6050577234392343535L;
+
+    protected final Map<String, Callable<?>> _values;
+
+    public InjectableValuesFromFactories() {
+        this(new HashMap<String, Callable<?>>());
+    }
+
+    public InjectableValuesFromFactories(Map<String, Callable<?>> values) {
+        _values = values;
+    }
+
+    public InjectableValuesFromFactories addValue(String key, Callable<?> value) {
+        Preconditions.checkNotNull(key);
+        Preconditions.checkNotNull(value);
+        _values.put(key, value);
+        return this;
+    }
+
+    public InjectableValuesFromFactories addValue(Class<?> classKey, Callable<?> value) {
+        Preconditions.checkNotNull(classKey);
+        Preconditions.checkNotNull(value);
+        _values.put(classKey.getName(), value);
+        return this;
+    }
+
+    @Override
+    public Object findInjectableValue(Object valueId, DeserializationContext ctxt, BeanProperty forProperty,
+            Object beanInstance) throws JsonMappingException {
+        if (!(valueId instanceof String)) {
+            ctxt.reportBadDefinition(ClassUtil.classOf(valueId), String.format(
+                    "Unrecognized inject value id type (%s), expecting String", ClassUtil.classNameOf(valueId)));
+        }
+        String key = (String) valueId;
+        Callable<?> callable = _values.get(key);
+        if (callable == null) {
+            throw new IllegalArgumentException(
+                    "No injectable id with value '" + key + "' found (for property '" + forProperty.getName() + "')");
+        }
+        Object ob;
+        try {
+            ob = callable.call();
+        } catch (Exception e) {
+            throw JsonMappingException.from(ctxt,
+                    "callable threw when creating value for property " + forProperty.getFullName(), e);
+        }
+        if (ob == null && !_values.containsKey(key)) {
+            throw new IllegalArgumentException(
+                    "No injectable id with value '" + key + "' found (for property '" + forProperty.getName() + "')");
+        }
+        return ob;
+    }
+}
