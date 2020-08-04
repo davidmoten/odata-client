04e3482a43637521fc4f917678371f09269a8f3b
David Moten <davidmoten@gmail.com>
2020-08-05 08:30:49 +1000

    edit GraphExplorerMain

--- odata-client-msgraph/src/test/java/com/github/davidmoten/msgraph/GraphExplorerMain.java
+++ odata-client-msgraph/src/test/java/com/github/davidmoten/msgraph/GraphExplorerMain.java
@@ -3,8 +3,6 @@ package com.github.davidmoten.msgraph;
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
-import com.github.davidmoten.odata.client.StreamProvider;
-
 import odata.msgraph.client.container.GraphService;
 import odata.msgraph.client.entity.DirectoryObject;
 import odata.msgraph.client.entity.FileAttachment;
@@ -16,22 +14,18 @@ public class GraphExplorerMain {
 
         GraphService client = MsGraph.explorer().build();
         {
-//            client //
-//                    .me() //
-//                    .messages() //
-//                    .stream() //
-//                    .peek(x -> System.out.println(x.getId().orElse("?"))) //
-//                    .flatMap(m -> m.getAttachments().metadataFull().stream()) //
-//                    .filter(x -> x instanceof FileAttachment) //
-//                    .map(x -> (FileAttachment) x) //
-//                    .limit(1) //
-//                    .peek(x -> System.out.println(x.getName().orElse("?"))) //
-//                    .map(x -> x.getStream().get().getBytes().length) //
-//                    .forEach(System.out::println);
-            String id = "AAMkAGVmMDEzMTM4LTZmYWUtNDdkNC1hMDZiLTU1OGY5OTZhYmY4OABGAAAAAAAiQ8W967B7TKBjgx9rVEURBwAiIsqMbYjsT5e-T7KzowPTAAAAAAEMAAAiIsqMbYjsT5e-T7KzowPTAALGDZ0eAAA=";
-//            client.me().messages(id).attachments().metadataFull().stream().filter(x -> x instanceof FileAttachment);
-            
-            StreamProvider att = client.me().messages(id).get().getStream().get();
+            client //
+                    .me() //
+                    .messages() //
+                    .stream() //
+                    .peek(x -> System.out.println(x.getId().orElse("?"))) //
+                    .flatMap(m -> m.getAttachments().metadataFull().stream()) //
+                    .filter(x -> x instanceof FileAttachment) //
+                    .map(x -> (FileAttachment) x) //
+                    .limit(1) //
+                    .peek(x -> System.out.println(x.getName().orElse("?"))) //
+                    .map(x -> x.getStream().get().getBytes().length) //
+                    .forEach(System.out::println);
         }
 
         System.exit(0);
