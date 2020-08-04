ebadd326b122b486a13a49ae6ca8f17b4b9c0bc8
David Moten <davidmoten@gmail.com>
2020-08-05 08:28:30 +1000

    fix GraphExplorer authentication that was broken by recent stream support change

--- odata-client-msgraph/src/test/java/com/github/davidmoten/msgraph/GraphExplorerMain.java
+++ odata-client-msgraph/src/test/java/com/github/davidmoten/msgraph/GraphExplorerMain.java
@@ -3,6 +3,8 @@ package com.github.davidmoten.msgraph;
 import java.util.Arrays;
 import java.util.concurrent.TimeUnit;
 
+import com.github.davidmoten.odata.client.StreamProvider;
+
 import odata.msgraph.client.container.GraphService;
 import odata.msgraph.client.entity.DirectoryObject;
 import odata.msgraph.client.entity.FileAttachment;
@@ -13,6 +15,26 @@ public class GraphExplorerMain {
     public static void main(String[] args) {
 
         GraphService client = MsGraph.explorer().build();
+        {
+//            client //
+//                    .me() //
+//                    .messages() //
+//                    .stream() //
+//                    .peek(x -> System.out.println(x.getId().orElse("?"))) //
+//                    .flatMap(m -> m.getAttachments().metadataFull().stream()) //
+//                    .filter(x -> x instanceof FileAttachment) //
+//                    .map(x -> (FileAttachment) x) //
+//                    .limit(1) //
+//                    .peek(x -> System.out.println(x.getName().orElse("?"))) //
+//                    .map(x -> x.getStream().get().getBytes().length) //
+//                    .forEach(System.out::println);
+            String id = "AAMkAGVmMDEzMTM4LTZmYWUtNDdkNC1hMDZiLTU1OGY5OTZhYmY4OABGAAAAAAAiQ8W967B7TKBjgx9rVEURBwAiIsqMbYjsT5e-T7KzowPTAAAAAAEMAAAiIsqMbYjsT5e-T7KzowPTAALGDZ0eAAA=";
+//            client.me().messages(id).attachments().metadataFull().stream().filter(x -> x instanceof FileAttachment);
+            
+            StreamProvider att = client.me().messages(id).get().getStream().get();
+        }
+
+        System.exit(0);
 
         DirectoryObject u = client //
                 .directoryObjects() //
@@ -24,11 +46,11 @@ public class GraphExplorerMain {
                 .stream() //
                 .findFirst() //
                 .get();
-        
+
         System.out.println(u);
-        
+
         System.exit(0);
-        
+
         client //
                 .me() //
                 .messages() //
@@ -51,8 +73,7 @@ public class GraphExplorerMain {
 
         System.exit(0);
 
-        String id = client.me().messages().select("id").stream().limit(1).findFirst().get().getId()
-                .get();
+        String id = client.me().messages().select("id").stream().limit(1).findFirst().get().getId().get();
 
         client //
                 .me() //
