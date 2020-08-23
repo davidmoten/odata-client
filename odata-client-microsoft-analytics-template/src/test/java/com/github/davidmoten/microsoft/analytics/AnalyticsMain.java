0c5abd0bac51181f5e6c4e04f9b6368788021a01
Dave Moten <davidmoten@gmail.com>
2020-08-23 16:06:24 +1000

    implement a path through the client builder for basic authentication

--- odata-client-microsoft-analytics-template/src/test/java/com/github/davidmoten/microsoft/analytics/AnalyticsMain.java
+++ odata-client-microsoft-analytics-template/src/test/java/com/github/davidmoten/microsoft/analytics/AnalyticsMain.java
@@ -1,12 +1,7 @@
 package com.github.davidmoten.microsoft.analytics;
 
-import java.nio.charset.StandardCharsets;
-import java.util.Base64;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import com.github.davidmoten.guavamini.Preconditions;
-import com.github.davidmoten.odata.client.RequestHeader;
+import com.github.davidmoten.msgraph.builder.MsGraphClientBuilder.UsernamePassword;
 
 import microsoft.vs.analytics.myorg.container.Container;
 
@@ -23,45 +18,27 @@ public class AnalyticsMain {
 		String project = "MyTestProject1";
 		String username = Preconditions.checkNotNull(System.getProperty("username"));
 		String password = Preconditions.checkNotNull(System.getProperty("password"));
-
 		String version = "3.0";
 		String baseUrl = "https://analytics.dev.azure.com/" + organization + "/" + project + "/_odata/v" + version;
+		
 		Container client = Analytics //
 				.service(Container.class) //
 				.baseUrl(baseUrl) //
-				.tenantName("123456.onmicrosoft.com") //
-				.clientId("not used") //
-				.clientSecret("not used") //
-				.authenticator((url, requestHeaders) -> {
-					// some streaming endpoints object to auth so don't add header
-					// if not on the base service
-					if (url.toExternalForm().startsWith(baseUrl)) {
-						// remove Authorization header if present
-						List<RequestHeader> list = requestHeaders //
-								.stream() //
-								.filter(rh -> !rh.name().equalsIgnoreCase("Authorization")) //
-								.collect(Collectors.toList());
-						// add basic auth request header
-						list.add(basicAuth(username, password));
-						return list;
-					} else {
-						return requestHeaders;
-					}
-				}).build();
+				.basicAuthentication(() -> UsernamePassword.create(username, password)) //
+				.build();
+		
+		// print out work items
 		client //
 				.workItems() //
 				.select("WorkItemId,State,CreatedDate") //
 				.stream() //
 				.filter(x -> x.getCreatedDate().isPresent()) //
-				.sorted((x,y) -> x.getCreatedDate().get().compareTo(y.getCreatedDate().get())) //
+				.sorted( (x,y) -> x.getCreatedDate().get().compareTo(y.getCreatedDate().get())) //
 				.map(x -> x.getWorkItemId() + ", " + x.getState() + ", " +  x.getCreatedDate()) //
 				.forEach(System.out::println);
-	}
-
-	private static RequestHeader basicAuth(String username, String password) {
-		String s = username + ":" + password;
-		String encoded = Base64.getEncoder().encodeToString(s.getBytes(StandardCharsets.UTF_8));
-		return RequestHeader.create("Authorization", "Basic " + encoded);
+		
+		// print out areas
+		client.areas().stream().forEach(System.out::println);
 	}
 
 }
