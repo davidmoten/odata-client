a74c33ee8cb2d0311d77b23f72ecd83cedb80da7
Dave Moten <davidmoten@gmail.com>
2020-08-24 08:19:16 +1000

    add more builder support to Analytics class

--- odata-client-msgraph-client-builder/src/main/java/com/github/davidmoten/microsoft/analytics/Analytics.java
+++ odata-client-msgraph-client-builder/src/main/java/com/github/davidmoten/microsoft/analytics/Analytics.java
@@ -1,11 +1,11 @@
 package com.github.davidmoten.microsoft.analytics;
 
 import java.lang.reflect.InvocationTargetException;
+import java.util.Optional;
 import java.util.function.Supplier;
 
 import com.github.davidmoten.guavamini.Preconditions;
 import com.github.davidmoten.msgraph.builder.MsGraphClientBuilder;
-import com.github.davidmoten.msgraph.builder.MsGraphClientBuilder.Builder3;
 import com.github.davidmoten.msgraph.builder.MsGraphClientBuilder.UsernamePassword;
 import com.github.davidmoten.odata.client.ClientException;
 import com.github.davidmoten.odata.client.Context;
@@ -27,44 +27,92 @@ public final class Analytics {
 	public static final class Builder<T extends HasContext> {
 
 		private final Class<T> serviceCls;
-		private String baseUrl;
+		private Optional<String> baseUrl = Optional.empty();
+		private Optional<String> organization = Optional.empty();
+		private Optional<String> project = Optional.empty();
+		private Optional<String> version;
 
 		Builder(Class<T> serviceClass) {
 			Preconditions.checkNotNull(serviceClass);
 			this.serviceCls = serviceClass;
 		}
-		
-		public Builder2<T> baseUrl(String baseUrl) {
+
+		public Builder3<T> baseUrl(String baseUrl) {
 			Preconditions.checkNotNull(baseUrl);
-			this.baseUrl = baseUrl;
+			this.baseUrl = Optional.of(baseUrl);
+			return new Builder3<T>(this);
+		}
+
+		public Builder2<T> organization(String organization) {
+			Preconditions.checkNotNull(organization);
+			this.organization = Optional.of(organization);
 			return new Builder2<T>(this);
 		}
-		
+
 	}
-	
+
 	public static final class Builder2<T extends HasContext> {
 
 		private final Builder<T> b;
 
-		public Builder2(Builder<T> b) {
+		Builder2(Builder<T> b) {
 			this.b = b;
 		}
-		
-		public Builder3<T> basicAuthentication(Supplier<UsernamePassword> usernamePassword) {
+
+		public Builder2<T> project(String project) {
+			Preconditions.checkNotNull(project);
+			b.project = Optional.of(project);
+			return this;
+		}
+
+		/**
+		 * Sets the version of the API. Expected values look like 'v1.0', 'v2.0',
+		 * 'v3.0', 'v4.0-preview'.
+		 * 
+		 * @param version version to use
+		 * @return this
+		 */
+		public Builder3<T> version(String version) {
+			Preconditions.checkNotNull(version);
+			b.version = Optional.of(version);
+			return new Builder3<T>(b);
+		}
+
+	}
+
+	public static final class Builder3<T extends HasContext> {
+
+		private static final String ANALYTICS_DEFAULT_VERSION = "v3.0";
+		private static final String ANALYTICS_BASE_URL = "https://analytics.dev.azure.com/";
+		private final Builder<T> b;
+
+		public Builder3(Builder<T> b) {
+			this.b = b;
+		}
+
+		public com.github.davidmoten.msgraph.builder.MsGraphClientBuilder.Builder3<T> basicAuthentication(
+				Supplier<UsernamePassword> usernamePassword) {
 			return createBuilder().basicAuthentication(usernamePassword);
-	    }
-		
-		public Builder3<T> basicAuthentication(String username, String password) {
+		}
+
+		public com.github.davidmoten.msgraph.builder.MsGraphClientBuilder.Builder3<T> basicAuthentication(
+				String username, String password) {
 			return basicAuthentication(() -> UsernamePassword.create(username, password));
 		}
-		
-		public  MsGraphClientBuilder.Builder<T> tenantName(String tenantName) {
+
+		public MsGraphClientBuilder.Builder<T> tenantName(String tenantName) {
 			return createBuilder().tenantName(tenantName);
 		}
-		
+
 		private MsGraphClientBuilder<T> createBuilder() {
+			String baseUrl = b.baseUrl.orElseGet(() -> {
+				return ANALYTICS_BASE_URL + b.organization.get() //
+						+ b.project.map(x -> "/" + x).orElse("") //
+						+ "/_odata" //
+						+ b.version.map(x -> "/" + x).orElse(ANALYTICS_DEFAULT_VERSION);
+			});
 			return new MsGraphClientBuilder<T> //
-			(b.baseUrl, context -> {
+			(baseUrl, context -> {
 				try {
 					return b.serviceCls.getConstructor(Context.class).newInstance(context);
 				} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
