3bed9a09d58c0a616f029cf8411a51f0913dd778
Dave Moten <davidmoten@gmail.com>
2020-08-24 07:50:13 +1000

    renamed analytics project

--- odata-client-microsoft-analytics/src/test/java/com/github/davidmoten/microsoft/analytics/AnalyticsMain.java
+++ odata-client-microsoft-analytics/src/test/java/com/github/davidmoten/microsoft/analytics/AnalyticsMain.java
@@ -1,6 +1,7 @@
 package com.github.davidmoten.microsoft.analytics;
 
 import com.github.davidmoten.guavamini.Preconditions;
+import com.github.davidmoten.odata.client.RequestOptions;
 
 import microsoft.vs.analytics.v3.myorg.container.Container;
 
@@ -18,13 +19,15 @@ public class AnalyticsMain {
 		String username = Preconditions.checkNotNull(System.getProperty("username"));
 		String password = Preconditions.checkNotNull(System.getProperty("password"));
 		String version = "3.0";
-		String baseUrl = "https://analytics.dev.azure.com/" + organization + "/" + project + "/_odata/v" + version;
+		String baseUrl = "https://analytics.dev.azure.com/" + organization + "/_odata/v" + version;
 		
 		Container client = Analytics //
 				.service(Container.class) //
 				.baseUrl(baseUrl) //
 				.basicAuthentication(username, password) //
 				.build();
+		
+		// System.out.println(client._custom().getString(baseUrl + "/$metadata", RequestOptions.EMPTY));
 		
 		// print out work items
 		client //
