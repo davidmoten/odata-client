b9f60b440183aacc5f52b89eff030e1cffc8e6db
Dave Moten <davidmoten@gmail.com>
2020-08-23 21:02:56 +1000

    add more metadata and generators for analytics services

--- odata-client-microsoft-analytics-template/src/main/odata/format-metadata.sh
+++ odata-client-microsoft-analytics-template/src/main/odata/format-metadata.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 set -e
+xmllint --format microsoft-analytics-v1-metadata.xml >/tmp/metadata.xml
+cp /tmp/metadata.xml microsoft-analytics-v1-metadata.xml
+xmllint --format microsoft-analytics-v2-metadata.xml >/tmp/metadata.xml
+cp /tmp/metadata.xml microsoft-analytics-v2-metadata.xml
 xmllint --format microsoft-analytics-v3-metadata.xml >/tmp/metadata.xml
 cp /tmp/metadata.xml microsoft-analytics-v3-metadata.xml
+xmllint --format microsoft-analytics-v4-metadata.xml >/tmp/metadata.xml
+cp /tmp/metadata.xml microsoft-analytics-v4-metadata.xml
+
