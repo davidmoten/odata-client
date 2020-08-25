2789a9d105235d75ce84aa7ca9191b44f4b6e9b6
Dave Moten <davidmoten@gmail.com>
2020-08-25 22:46:09 +1000

    Email send helper dev

---
+++ odata-client-msgraph/src/main/java/com/github/davidmoten/msgraph/Email.java
@@ -0,0 +1,337 @@
+package com.github.davidmoten.msgraph;
+
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+
+import com.github.davidmoten.odata.client.Retries;
+import com.github.davidmoten.odata.client.StreamUploaderChunked;
+import com.github.davidmoten.odata.client.internal.Util;
+
+import odata.msgraph.client.complex.AttachmentItem;
+import odata.msgraph.client.complex.EmailAddress;
+import odata.msgraph.client.complex.ItemBody;
+import odata.msgraph.client.complex.Recipient;
+import odata.msgraph.client.container.GraphService;
+import odata.msgraph.client.entity.FileAttachment;
+import odata.msgraph.client.entity.Message;
+import odata.msgraph.client.entity.request.MailFolderRequest;
+import odata.msgraph.client.enums.AttachmentType;
+import odata.msgraph.client.enums.BodyType;
+
+public final class Email {
+
+	public static Builder mailbox(String mailbox) {
+		return new Builder(mailbox);
+	}
+
+	public static final class Builder {
+
+		private final String mailbox;
+		private String subject;
+		private String body;
+		private String from;
+		private final List<String> to = new ArrayList<>();
+		private final List<String> cc = new ArrayList<>();
+		private final List<String> bcc = new ArrayList<>();
+		private String draftFolder = "Drafts";
+		private BodyType bodyType;
+		private final List<BuilderAttachment> attachments = new ArrayList<>();
+
+		Builder(String mailbox) {
+			this.mailbox = mailbox;
+			this.from = mailbox;
+		}
+
+		Builder2 subject(String subject) {
+			this.subject = subject;
+			return new Builder2(this);
+		}
+
+	}
+
+	public static final class Builder11 {
+		private final Builder b;
+
+		Builder11(Builder b) {
+			this.b = b;
+		}
+
+		public Builder2 bodyType(BodyType bodyType) {
+			b.bodyType = bodyType;
+			return new Builder2(b);
+		}
+	}
+
+	public static final class Builder2 {
+
+		private final Builder b;
+
+		Builder2(Builder b) {
+			this.b = b;
+		}
+
+		Builder4 body(String body) {
+			b.body = body;
+			return new Builder4(b);
+		}
+	}
+
+	public static final class Builder4 {
+
+		private static final int ATTACHMENT_SIZE_THRESHOLD = 3000000;
+		private final Builder b;
+
+		Builder4(Builder b) {
+			this.b = b;
+		}
+
+		Builder4 from(String emailAddress) {
+			b.from = emailAddress;
+			return new Builder4(b);
+		}
+		
+		Builder4 to(String... emailAddresses) {
+			return to(Arrays.asList(emailAddresses));
+		}
+
+		Builder4 to(Iterable<String> emailAddresses) {
+			for (String a : emailAddresses) {
+				b.to.add(a);
+			}
+			return this;
+		}
+
+		Builder4 cc(String... emailAddresses) {
+			return cc(Arrays.asList(emailAddresses));
+		}
+
+		Builder4 cc(Iterable<String> emailAddresses) {
+			for (String a : emailAddresses) {
+				b.cc.add(a);
+			}
+			return this;
+		}
+
+		Builder4 bcc(String... emailAddresses) {
+			return bcc(Arrays.asList(emailAddresses));
+		}
+
+		Builder4 bcc(List<String> emailAddresses) {
+			for (String a : emailAddresses) {
+				b.bcc.add(a);
+			}
+			return this;
+		}
+
+		Builder4 saveDraftToFolder(String draftFolder) {
+			b.draftFolder = draftFolder;
+			return this;
+		}
+
+		BuilderAttachment attachmentName(String name) {
+			return new BuilderAttachment(this, name);
+		}
+		
+		Builder6 attachment(File file) {
+			return attachmentName(file.getName()).file(file);
+		}
+
+		void send(GraphService client) {
+			MailFolderRequest drafts = client //
+					.users(b.mailbox) //
+					.mailFolders(b.draftFolder);
+			odata.msgraph.client.entity.Message.Builder builder = Message //
+					.builderMessage() //
+					.subject(b.subject) //
+					.body(ItemBody.builder() //
+							.content(b.body) //
+							.contentType(b.bodyType) //
+							.build()) //
+					.from(Recipient.builder() //
+							.emailAddress(EmailAddress //
+									.builder() //
+									.address(b.from) //
+									.build()) //
+							.build());
+			if (!b.to.isEmpty()) {
+				builder = builder.toRecipients(recipients(b.to));
+			}
+			if (!b.cc.isEmpty()) {
+				builder = builder.ccRecipients(recipients(b.cc));
+			}
+			if (!b.bcc.isEmpty()) {
+				builder = builder.ccRecipients(recipients(b.bcc));
+			}
+			Message m = drafts.messages().post(builder.build());
+
+			// upload attachments
+			for (BuilderAttachment a : b.attachments) {
+				// Upload attachment to the new mail
+				// We use different methods depending on the size of the attachment
+				// because will fail if doesn't match the right size window
+				if (a.file != null) {
+					a.length = a.file.length();
+				}
+				if (a.length < ATTACHMENT_SIZE_THRESHOLD) {
+					final byte[] contentBytes;
+					if (a.file != null) {
+						try {
+							contentBytes = Files.readAllBytes(a.file.toPath());
+						} catch (IOException e) {
+							throw new UncheckedIOException(e);
+						}
+					} else {
+						contentBytes = Util.read(a.inputStream);
+					}
+					client.users(b.mailbox) //
+							.messages(m.getId().get()) //
+							.attachments() //
+							.post(FileAttachment.builderFileAttachment() //
+									.name(a.name) //
+									.contentBytes(contentBytes) //
+									.contentType(a.contentMimeType) //
+									.build());
+				} else {
+					AttachmentItem ai = AttachmentItem //
+							.builder() //
+							.attachmentType(AttachmentType.FILE) //
+							.contentType(a.contentMimeType) //
+							.name(a.name) //
+							.size(a.length) //
+							.build();
+
+					StreamUploaderChunked uploader = client //
+							.users(a.sender.b.mailbox) //
+							.messages(m.getId().get()) //
+							.attachments() //
+							.createUploadSession(ai) //
+							.get() //
+							.putChunked();
+					if (a.readTimeoutMs == -1) {
+						uploader = uploader.readTimeout(a.readTimeoutMs, TimeUnit.MILLISECONDS);
+					}
+					if (a.file != null) {
+						uploader.upload(a.file, a.chunkSize, a.retries);
+					} else {
+						uploader.upload(a.inputStream, a.length, a.chunkSize, a.retries);
+					}
+				}
+			}
+			m.send().call();
+		}
+	}
+
+	private static List<Recipient> recipients(List<String> emailAddresses) {
+		return emailAddresses.stream().map(x -> recipient(x)) //
+				.collect(Collectors.toList());
+	}
+
+	private static Recipient recipient(String emailAddress) {
+		return Recipient.builder() //
+				.emailAddress(EmailAddress.builder() //
+						.address(emailAddress) //
+						.build()) //
+				.build();
+	}
+
+	public static final class BuilderAttachment {
+
+		private long readTimeoutMs = -1; // use default
+		private final String name;
+		private final Builder4 sender;
+		private String contentMimeType = "application/octet-stream";
+		private File file;
+		private InputStream inputStream;
+		private long length;
+		private int chunkSize = 512 * 1024;
+		private Retries retries = Retries.NONE;
+
+		public BuilderAttachment(Builder4 sender, String name) {
+			this.sender = sender;
+			this.name = name;
+		}
+
+		public Builder6 file(File file) {
+			this.file = file;
+			return new Builder6(this);
+		}
+
+		public Builder5 inputStream(InputStream in) {
+			this.inputStream = in;
+			return new Builder5(this);
+		}
+
+		public Builder6 bytes(byte[] bytes) {
+			return inputStream(new ByteArrayInputStream(bytes)).length(bytes.length);
+		}
+
+		public Builder6 contentTextUtf8(String text) {
+			return bytes(text.getBytes(StandardCharsets.UTF_8)).contentMimeType("text/plain");
+		}
+
+	}
+
+	public static final class Builder5 {
+
+		private final BuilderAttachment attachment;
+
+		Builder5(BuilderAttachment attachment) {
+			this.attachment = attachment;
+		}
+
+		public Builder6 length(long length) {
+			attachment.length = length;
+			return new Builder6(attachment);
+		}
+	}
+
+	public static final class Builder6 {
+
+		private final BuilderAttachment attachment;
+
+		public Builder6(BuilderAttachment attachment) {
+			this.attachment = attachment;
+		}
+
+		public Builder6 contentMimeType(String mimeType) {
+			attachment.contentMimeType = mimeType;
+			return this;
+		}
+
+		public Builder6 readTimeout(long duration, TimeUnit unit) {
+			attachment.readTimeoutMs = unit.toMillis(duration);
+			return this;
+		}
+
+		public Builder6 chunkSize(int chunkSize) {
+			attachment.chunkSize = chunkSize;
+			return this;
+		}
+
+		public Builder6 retries(Retries retries) {
+			attachment.retries = retries;
+			return this;
+		}
+
+		public BuilderAttachment attachmentName(String name) {
+			attachment.sender.b.attachments.add(attachment);
+			return new BuilderAttachment(attachment.sender, name);
+		}
+
+		public void send(GraphService client) {
+			attachment.sender.b.attachments.add(attachment);
+			attachment.sender.send(client);
+		}
+
+	}
+}
--- odata-client-runtime/src/main/java/com/github/davidmoten/odata/client/internal/Util.java
+++ odata-client-runtime/src/main/java/com/github/davidmoten/odata/client/internal/Util.java
@@ -13,84 +13,94 @@ import com.github.davidmoten.odata.client.ODataType;
 
 public final class Util {
 
-    public static String readString(InputStream in, Charset charset) throws IOException {
-        ByteArrayOutputStream result = new ByteArrayOutputStream();
-        byte[] buffer = new byte[1024];
-        int length;
-        while ((length = in.read(buffer)) != -1) {
-            result.write(buffer, 0, length);
-        }
-        return new String(result.toByteArray(), charset);
-    }
+	public static String readString(InputStream in, Charset charset) throws IOException {
+		ByteArrayOutputStream result = new ByteArrayOutputStream();
+		byte[] buffer = new byte[1024];
+		int length;
+		while ((length = in.read(buffer)) != -1) {
+			result.write(buffer, 0, length);
+		}
+		return new String(result.toByteArray(), charset);
+	}
 
-    public static String trimTrailingSlash(String url) {
-        if (!url.isEmpty() && url.charAt(url.length() - 1) == '/') {
-            return url.substring(0, url.length() - 2);
-        } else {
-            return url;
-        }
-    }
-    
-    @SuppressWarnings("unchecked")
+	public static String trimTrailingSlash(String url) {
+		if (!url.isEmpty() && url.charAt(url.length() - 1) == '/') {
+			return url.substring(0, url.length() - 2);
+		} else {
+			return url;
+		}
+	}
+
+	@SuppressWarnings("unchecked")
 	public static <T extends ODataType> String odataTypeName(Class<T> cls) {
-        try {
-        	Constructor<?> c = null;
-        	Constructor<?>[] constructors = cls.getDeclaredConstructors();
-            for (Constructor<?> constructor : constructors) {
-            	if (constructor.getParameterCount() == 0) {
-            	    constructor.setAccessible(true);
-            	    c = constructor;
-            	}
-            }
-            T o = (T) c.newInstance();
-            return o.odataTypeName();
-        } catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
-                | SecurityException e) {
-            throw new ClientException(e);
-        }
-    }
-    
-    @SuppressWarnings("unchecked")
-	public static <T> String odataTypeNameFromAny(Class<T> cls) {
-        if (ODataType.class.isAssignableFrom(cls)) {
-            return odataTypeName((Class<? extends ODataType>) cls);
-        } else {
-            return EdmSchemaInfo.INSTANCE.getTypeWithNamespaceFromClass(cls);
-        }
-    }
-    
-    /**
-     * Reads size bytes into buffer from InputStream if end of stream not reached.
-     * Returns the number of bytes read (which will be {@code size} if end of stream
-     * not reached).
-     * 
-     * @param in
-     *            input stream
-     * @param buffer
-     *            destination array to read into
-     * @param size
-     *            number of bytes to read if available
-     * @return number of bytes read
-     */
-    // TODO unit test
-    public static int readFully(InputStream in, byte[] buffer, int size) {
-        int len = 0;
-        while (len < size) {
-            try {
-                int numRead = in.read(buffer, len, size - len);
-                if (numRead == -1) {
-                    break;
-                } else {
-                    len += numRead;
-                }
-            } catch (IOException e) {
-                // this is unrecoverable because we cannot reread from the InputStream
-                // TODO provide a Supplier<InputStream> parameter?
-                throw new UncheckedIOException(e);
-            }
-        }
-        return len;
-    }
+		try {
+			Constructor<?> c = null;
+			Constructor<?>[] constructors = cls.getDeclaredConstructors();
+			for (Constructor<?> constructor : constructors) {
+				if (constructor.getParameterCount() == 0) {
+					constructor.setAccessible(true);
+					c = constructor;
+				}
+			}
+			T o = (T) c.newInstance();
+			return o.odataTypeName();
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException
+				| SecurityException e) {
+			throw new ClientException(e);
+		}
+	}
 
+	@SuppressWarnings("unchecked")
+	public static <T> String odataTypeNameFromAny(Class<T> cls) {
+		if (ODataType.class.isAssignableFrom(cls)) {
+			return odataTypeName((Class<? extends ODataType>) cls);
+		} else {
+			return EdmSchemaInfo.INSTANCE.getTypeWithNamespaceFromClass(cls);
+		}
+	}
+
+	/**
+	 * Reads size bytes into buffer from InputStream if end of stream not reached.
+	 * Returns the number of bytes read (which will be {@code size} if end of stream
+	 * not reached).
+	 * 
+	 * @param in     input stream
+	 * @param buffer destination array to read into
+	 * @param size   number of bytes to read if available
+	 * @return number of bytes read
+	 */
+	// TODO unit test
+	public static int readFully(InputStream in, byte[] buffer, int size) {
+		int len = 0;
+		while (len < size) {
+			try {
+				int numRead = in.read(buffer, len, size - len);
+				if (numRead == -1) {
+					break;
+				} else {
+					len += numRead;
+				}
+			} catch (IOException e) {
+				// this is unrecoverable because we cannot reread from the InputStream
+				// TODO provide a Supplier<InputStream> parameter?
+				throw new UncheckedIOException(e);
+			}
+		}
+		return len;
+	}
+
+	public static byte[] read(InputStream in) {
+		ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+		int n;
+		byte[] buffer = new byte[8192];
+		try {
+			while ((n = in.read(buffer)) != -1) {
+				bytes.write(buffer, 0, n);
+			}
+			return bytes.toByteArray();
+		} catch (IOException e) {
+			throw new UncheckedIOException(e);
+		}
+	}
 
 }
