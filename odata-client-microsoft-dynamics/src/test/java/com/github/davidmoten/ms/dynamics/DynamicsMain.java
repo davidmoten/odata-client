7dfa6203c4086690127492fcc2a4b3d59b3fd8c0
Dave Moten <davidmoten@gmail.com>
2020-09-01 19:45:40 +1000

    add DynamicsMain

---
+++ odata-client-microsoft-dynamics/src/test/java/com/github/davidmoten/ms/dynamics/DynamicsMain.java
@@ -0,0 +1,25 @@
+package com.github.davidmoten.ms.dynamics;
+
+import java.util.concurrent.TimeUnit;
+
+import com.github.davidmoten.microsoft.analytics.Analytics;
+
+import microsoft.dynamics.crm.container.System;
+
+public class DynamicsMain {
+
+    public static void main(String[] args) {
+        System client = Analytics.service(System.class) //
+                .baseUrl("https://solution.api.crm4.dynamics.com/api/data/v9.1/") //
+                .tenantName("TENANT_NAME") //
+                .clientId("CLIENT_ID") //
+                .clientSecret("CLIENT_SECRET") //
+                .refreshBeforeExpiry(5, TimeUnit.MINUTES) //
+                .build();
+        // now use client
+        client.applicationusers().metadataMinimal().stream().limit(10)
+                .forEach(java.lang.System.out::println);
+
+    }
+
+}
