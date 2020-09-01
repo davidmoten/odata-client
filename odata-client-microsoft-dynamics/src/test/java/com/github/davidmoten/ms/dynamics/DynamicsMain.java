4ec339c1f57d7cc6026789a7c52203546dbb0fca
Dave Moten <davidmoten@gmail.com>
2020-09-01 19:53:05 +1000

    update DynamicsMain

--- odata-client-microsoft-dynamics/src/test/java/com/github/davidmoten/ms/dynamics/DynamicsMain.java
+++ odata-client-microsoft-dynamics/src/test/java/com/github/davidmoten/ms/dynamics/DynamicsMain.java
@@ -9,13 +9,17 @@ import microsoft.dynamics.crm.container.System;
 public class DynamicsMain {
 
     public static void main(String[] args) {
-        System client = Analytics.service(System.class) //
-                .baseUrl("https://solution.api.crm4.dynamics.com/api/data/v9.1/") //
+        
+        // build client
+        System client = Analytics //
+                .service(System.class) //
+                .baseUrl("https://SOLUTION.api.crm4.dynamics.com/api/data/v9.1/") //
                 .tenantName("TENANT_NAME") //
                 .clientId("CLIENT_ID") //
                 .clientSecret("CLIENT_SECRET") //
                 .refreshBeforeExpiry(5, TimeUnit.MINUTES) //
                 .build();
+        
         // now use client
         client.applicationusers().metadataMinimal().stream().limit(10)
                 .forEach(java.lang.System.out::println);
