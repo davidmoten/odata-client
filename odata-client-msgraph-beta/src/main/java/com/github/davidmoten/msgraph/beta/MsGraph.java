d7c9681179e45bd8100474e874713c02292719c7
Dave Moten <davidmoten@gmail.com>
2020-09-04 11:35:46 +1000

    rename artifact odata-client-msgraph-client-builder to odata-client-microsoft-client-builder

--- odata-client-msgraph-beta/src/main/java/com/github/davidmoten/msgraph/beta/MsGraph.java
+++ odata-client-msgraph-beta/src/main/java/com/github/davidmoten/msgraph/beta/MsGraph.java
@@ -2,10 +2,10 @@ package com.github.davidmoten.msgraph.beta;
 
 import java.util.concurrent.TimeUnit;
 
+import com.github.davidmoten.microsoft.authentication.GraphConstants;
 import com.github.davidmoten.microsoft.client.builder.MicrosoftClientBuilder;
 import com.github.davidmoten.microsoft.client.builder.MicrosoftClientBuilder.Builder3;
 import com.github.davidmoten.msgraph.builder.GraphExplorerHttpService;
-import com.github.davidmoten.msgraph.builder.internal.MsGraphConstants;
 
 import odata.msgraph.client.beta.container.GraphService;
 
@@ -20,8 +20,8 @@ public final class MsGraph {
     public static Builder3<GraphService> tenantName(String tenantName) {
         return new MicrosoftClientBuilder<GraphService>(MSGRAPH_BETA_BASE_URL, GraphService::new) //
                 .tenantName(tenantName) //
-                .resource(MsGraphConstants.RESOURCE_MS_GRAPH) //
-                .scope(MsGraphConstants.SCOPE_MS_GRAPH_DEFAULT);
+                .resource(GraphConstants.RESOURCE_MS_GRAPH) //
+                .scope(GraphConstants.SCOPE_MS_GRAPH_DEFAULT);
     }
 
     public static com.github.davidmoten.microsoft.client.builder.MicrosoftClientBuilder.Builder5<GraphService> explorer() {
--- odata-client-msgraph/src/main/java/com/github/davidmoten/msgraph/MsGraph.java
+++ odata-client-msgraph/src/main/java/com/github/davidmoten/msgraph/MsGraph.java
@@ -2,10 +2,10 @@ package com.github.davidmoten.msgraph;
 
 import java.util.concurrent.TimeUnit;
 
+import com.github.davidmoten.microsoft.authentication.GraphConstants;
 import com.github.davidmoten.microsoft.client.builder.MicrosoftClientBuilder;
 import com.github.davidmoten.microsoft.client.builder.MicrosoftClientBuilder.Builder5;
 import com.github.davidmoten.msgraph.builder.GraphExplorerHttpService;
-import com.github.davidmoten.msgraph.builder.internal.MsGraphConstants;
 
 import odata.msgraph.client.container.GraphService;
 
@@ -21,8 +21,8 @@ public final class MsGraph {
         return new MicrosoftClientBuilder<> 
                 (MSGRAPH_1_0_BASE_URL, GraphService::new) //
                 .tenantName(tenantName) //
-                .resource(MsGraphConstants.RESOURCE_MS_GRAPH) //
-                .scope(MsGraphConstants.SCOPE_MS_GRAPH_DEFAULT);
+                .resource(GraphConstants.RESOURCE_MS_GRAPH) //
+                .scope(GraphConstants.SCOPE_MS_GRAPH_DEFAULT);
     }
 
     public static Builder5<GraphService> explorer() {
