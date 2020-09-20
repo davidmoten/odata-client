b36fb8e0401f203b3d01dcc1e0214239636b6246
Dave Moten <davidmoten@gmail.com>
2020-09-20 21:31:10 +1000

    fix msgraph metadata

--- odata-client-msgraph/update-metadata.sh
+++ odata-client-msgraph/update-metadata.sh
@@ -10,6 +10,9 @@ cp /tmp/beta.metadata.2 ../odata-client-msgraph-beta/src/main/odata/msgraph-beta
 sed -i 's/<EntityType Name="itemAttachment" BaseType="graph.attachment">/<EntityType Name="itemAttachment" BaseType="graph.attachment" HasStream="true">/g' ../odata-client-generator/src/main/odata/msgraph-metadata.xml
 sed -i 's/<EntityType Name="message" BaseType="graph.outlookItem" OpenType="true">/<EntityType Name="message" BaseType="graph.outlookItem" OpenType="true" HasStream="true">/g' ../odata-client-generator/src/main/odata/msgraph-metadata.xml
 sed -i 's/<EntityType Name="chatMembersNotificationAudience"\/>/<!-- commented out due no keys: <EntityType Name="chatMembersNotificationAudience"\/> -->/g' ../odata-client-msgraph-beta/src/main/odata/msgraph-beta-metadata.xml
+sed -i 's/<EntityType Name="auditLogRoot">/<EntityType Name="auditLogRoot" BaseType="graph.entity">/g' ../odata-client-generator/src/main/odata/msgraph-metadata.xml
+sed -i 's/<EntityType Name="identityContainer">/<EntityType Name="identityContainer" BaseType="graph.entity">/g' ../odata-client-generator/src/main/odata/msgraph-metadata.xml
+sed -i 's/<EntityType Name="directory">/<EntityType Name="directory" BaseType="graph.entity">/g' ../odata-client-generator/src/main/odata/msgraph-metadata.xml
 
 ## not required, Microsoft removed this item from metadata
 #sed -i 's/<Singleton Name="settings" Type="microsoft.graph.entitlementManagementSettings"\/>/<Singleton Name="entitlementManagementSettings" Type="microsoft.graph.entitlementManagementSettings"\/>/g' ../odata-client-msgraph-beta/src/main/odata/msgraph-beta-metadata.xml
