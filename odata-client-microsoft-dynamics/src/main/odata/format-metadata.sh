eeb0ab7ff45f16b6423547df9e2c89913b37ef7e
Dave Moten <davidmoten@gmail.com>
2020-09-21 10:32:03 +1000

    add crm6 sample dynamics metadata, not processing yet due to missing EntitySet owners

--- odata-client-microsoft-dynamics/src/main/odata/format-metadata.sh
+++ odata-client-microsoft-dynamics/src/main/odata/format-metadata.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
-xmllint --format microsoft-dynamics-metadata.xml >/tmp/metadata.xml
-cp /tmp/metadata.xml microsoft-dynamics-metadata.xml
+xmllint --format microsoft-dynamics-crm4-v9.1-metadata.xml >/tmp/metadata.xml
+cp /tmp/metadata.xml microsoft-dynamics-crm4-v9.1-metadata.xml 
+xmllint --format microsoft-dynamics-crm6-v9.1-metadata.xml >/tmp/metadata.xml
+cp /tmp/metadata.xml microsoft-dynamics-crm6-v9.1-metadata.xml 
+
 
