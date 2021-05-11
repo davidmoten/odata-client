a3a10257048a98cdf00d8af4ec0f76831f484b55
David Moten <davidmoten@gmail.com>
2021-05-11 16:02:44 +1000

    improve release.sh

--- release.sh
+++ release.sh
@@ -16,7 +16,7 @@ git tag -a $VERSION -m "$VERSION"
 git push origin $VERSION
 mvn versions:set -DnextSnapshot=true
 git commit -am "set versions to next snapshot"
-git push
+git push origin master
 echo =========================================================================
 echo At this point the tag has been pushed to the git repository on GitHub
 echo and you should go to the GitHub UI to indicate that that tag is the 
